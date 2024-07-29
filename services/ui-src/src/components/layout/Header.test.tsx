8cbce16bb33df06449c13167379f739f704716b1
Rocio De Santiago <rociodesantiago@CoformaSantiago.lan>
2024-07-29 16:27:25 -0500

    add test for menu and logout

--- services/ui-src/src/components/layout/Header.test.tsx
+++ services/ui-src/src/components/layout/Header.test.tsx
@@ -1,6 +1,6 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { RouterWrappedComponent } from "utils/testing/setupJest";
-import { Header } from "components";
+import { Header, MenuOption } from "components";
 import { testA11y } from "utils/testing/commonTests";
 
 const headerComponent = (
@@ -30,6 +30,36 @@ describe("<Header />", () => {
 
     test("Menu button is visible", () => {
       expect(screen.getByAltText("Arrow down")).toBeVisible();
+    });
+
+    test("Renders My Account and is clickable", () => {
+      render(
+        <MenuOption
+          text={"My Account"}
+          icon={"icon_arrrow_down.png"}
+          altText={"Arrow down"}
+        />
+      );
+
+      const menuButton = screen.getByRole("button", { name: /my account/i });
+      expect(menuButton).toBeInTheDocument();
+
+      fireEvent.click(menuButton);
+    });
+
+    test("Logs out user", () => {
+      render(
+        <MenuOption
+          text={"Log Out"}
+          icon={"icon_arrow_right_square.png"}
+          altText={"Logout"}
+        />
+      );
+
+      const logoutButton = screen.getByRole("img", { name: /Logout/i });
+      expect(logoutButton).toBeInTheDocument();
+
+      fireEvent.click(logoutButton);
     });
   });
 
