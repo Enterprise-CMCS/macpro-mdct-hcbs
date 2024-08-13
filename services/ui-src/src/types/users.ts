6105cf4d13129f425434d477e1f2a87b250e472c
Brendan Myers <6362494+BearHanded@users.noreply.github.com>
2024-08-13 11:47:09 -0400

    Remove dash in idm role name (#21)

--- services/ui-src/src/types/users.ts
+++ services/ui-src/src/types/users.ts
@@ -2,11 +2,11 @@
 
 // TODO: Finalize with IDM
 export enum UserRoles {
-  ADMIN = "mdct-hcbs-bor", // "MDCT HCBS Business Owner Representative"
-  APPROVER = "mdct-hcbs-appr", // "MDCT HCBS Approver"
-  HELP_DESK = "mdct-hcbs-hd", // "MDCT HCBS Help Desk"
-  INTERNAL = "mdct-hcbs-internal-user", // "MDCT HCBS Internal User"
-  STATE_USER = "mdct-hcbs-state-user", // "MDCT HCBS State User"
+  ADMIN = "mdcthcbs-bor", // "MDCT HCBS Business Owner Representative"
+  APPROVER = "mdcthcbs-appr", // "MDCT HCBS Approver"
+  HELP_DESK = "mdcthcbs-hd", // "MDCT HCBS Help Desk"
+  INTERNAL = "mdcthcbs-internal-user", // "MDCT HCBS Internal User"
+  STATE_USER = "mdcthcbs-state-user", // "MDCT HCBS State User"
 }
 
 export interface HcbsUser {
