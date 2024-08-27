e0ef6c5ae673b63594e4328bfcae4e21c8e946c0
Rocio De Santiago <rociodesantiago@CoformaSantiago.lan>
2024-08-27 13:29:38 -0500

    update Alert test

--- services/ui-src/src/components/alerts/Alert.test.tsx
+++ services/ui-src/src/components/alerts/Alert.test.tsx
@@ -6,15 +6,29 @@ const alertComponent = (
   <Alert
     title="Test alert!"
     description="This is for testing."
-    link="test-link"
-    data-testid="test-alert"
+    link="https://example.com"
   />
 );
 
 describe("<Alert />", () => {
   test("Alert is visible", () => {
     render(alertComponent);
-    expect(screen.getByTestId("test-alert")).toBeVisible();
+    expect(screen.getByRole("alert")).toBeVisible();
+  });
+  test("Alert link is visible", () => {
+    render(alertComponent);
+    expect(
+      screen.getByRole("link", { name: "https://example.com" })
+    ).toHaveAttribute("href", "https://example.com");
+  });
+  test("Alert text exists", () => {
+    render(alertComponent);
+    expect(screen.getByRole("alert")).toHaveTextContent("This is for testing.");
+  });
+
+  test("Alert image exists", () => {
+    render(alertComponent);
+    expect(screen.getByRole("img", { name: "Alert" })).toBeVisible();
   });
 
   testA11y(alertComponent);
