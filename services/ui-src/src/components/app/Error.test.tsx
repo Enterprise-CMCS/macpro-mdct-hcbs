740ac5f9241b4fa37fa063295b170a68a2189c95
Rocio De Santiago <rociodesantiago@CoformaSantiago.lan>
2024-08-28 17:04:22 -0500

    update error test

--- services/ui-src/src/components/app/Error.test.tsx
+++ services/ui-src/src/components/app/Error.test.tsx
@@ -5,9 +5,11 @@ import { testA11y } from "utils/testing/commonTests";
 const errorView = <Error />;
 
 describe("<Error />", () => {
-  test("Check that Error page renders", () => {
-    const { getByTestId } = render(errorView);
-    expect(getByTestId("error-view")).toBeVisible();
+  test("Check that Error message renders", () => {
+    const { getByRole } = render(errorView);
+    expect(getByRole("heading", { level: 2 })).toHaveTextContent(
+      "Sorry! An error has occurred."
+    );
   });
 
   testA11y(errorView);
