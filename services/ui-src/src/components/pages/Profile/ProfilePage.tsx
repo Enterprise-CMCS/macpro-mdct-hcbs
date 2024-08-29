2c4f4b3706d43b31383ae378b218235a821b6d27
Rocio De Santiago <rociodesantiago@CoformaSantiago.lan>
2024-08-29 10:29:12 -0500

    remove data-testids from profile page

--- services/ui-src/src/components/pages/Profile/ProfilePage.tsx
+++ services/ui-src/src/components/pages/Profile/ProfilePage.tsx
@@ -24,7 +24,7 @@ export const ProfilePage = () => {
   };
 
   return (
-    <PageTemplate sx={sx.layout} data-testid="profile-view">
+    <PageTemplate sx={sx.layout}>
       <Heading as="h1" sx={sx.headerText}>
         {intro.header}
       </Heading>
@@ -37,11 +37,7 @@ export const ProfilePage = () => {
       </Text>
       <Table content={tableContent} variant="striped" sxOverride={sx.table} />
       {userIsAdmin && (
-        <Button
-          sx={sx.adminButton}
-          onClick={() => navigate("/admin")}
-          data-testid="banner-admin-button"
-        >
+        <Button sx={sx.adminButton} onClick={() => navigate("/admin")}>
           Banner Editor
         </Button>
       )}
