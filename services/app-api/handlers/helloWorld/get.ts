c1529e7e8f6a513081a9ab88e580ebc8ed96f206
benmartin-coforma <Ben.Martin@coforma.io>
2024-09-23 10:35:55 -0600

    Remove prototype HelloWorld endpoint

--- services/app-api/handlers/helloWorld/get.ts
+++
@@ -1,6 +0,0 @@
-import handler from "../../libs/handler-lib";
-import { ok } from "../../libs/response-lib";
-
-export const getHelloWorld = handler(async (_event) => {
-  return ok("Hello World!");
-});
