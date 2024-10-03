ea2e07ee0890194606ccaae2b10a5947ae923190
Ajaita Saini <ajaita.saini@coforma.io>
2024-10-02 21:51:03 -0400

    wip

--- services/ui-src/src/components/fields/TextField.tsx
+++ services/ui-src/src/components/fields/TextField.tsx
@@ -1,92 +1,57 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useFormContext } from "react-hook-form";
 import { TextField as CmsdsTextField } from "@cmsgov/design-system";
 import { Box } from "@chakra-ui/react";
 import { labelTextWithOptional, parseCustomHtml } from "utils";
-import { AnyObject, CustomHtmlElement } from "types";
+import { TextboxTemplate } from "../../types/report";
+import { PageElementProps } from "../report/Elements";
 
-export const TextField = ({
-  name,
-  label,
-  hint,
-  placeholder,
-  sxOverride,
-  nested,
-  validateOnRender,
-  styleAsOptional,
-  clear,
-  ...props
-}: Props) => {
+export const TextField = (props: PageElementProps) => {
+  const textbox = props.element as TextboxTemplate;
   const defaultValue = "";
   const [displayValue, setDisplayValue] = useState<string>(defaultValue);
 
   // get form context and register field
   const form = useFormContext();
-  const fieldIsRegistered = name in form.getValues();
+  const inputName = "answers.1.2"; // TODO: id based
 
   useEffect(() => {
-    if (!fieldIsRegistered && !validateOnRender) {
-      form.register(name);
-    } else if (validateOnRender) {
-      form.trigger(name);
-    }
+    form.register(inputName);
   }, []);
 
-  // set initial display value to form state field value or hydration value
-  const hydrationValue = props?.hydrate || defaultValue;
+  const onChangeHandler = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const { name, value } = event.target;
+    setDisplayValue(value);
+    form.setValue(name, value, { shouldValidate: true });
+  };
 
-  useEffect(() => {
-    // if form state has value for field, set as display value
-    const fieldValue = form.getValues(name);
-    if (fieldValue) {
-      setDisplayValue(fieldValue);
-    }
-    // else set hydrationValue or defaultValue as display value
-    else if (hydrationValue) {
-      if (clear) {
-        setDisplayValue(defaultValue);
-        form.setValue(name, defaultValue);
-      } else {
-        setDisplayValue(hydrationValue);
-        form.setValue(name, hydrationValue, { shouldValidate: true });
-      }
-    }
-  }, [hydrationValue]); // only runs on hydrationValue fetch/update
+  const onBlurHandler = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    form.setValue(inputName, event.target.value);
+  };
 
   // prepare error message, hint, and classes
   const formErrorState = form?.formState?.errors;
-  const errorMessage = formErrorState?.[name]?.message;
-  const parsedHint = hint && parseCustomHtml(hint);
-  const nestedChildClasses = nested ? "nested ds-c-choice__checkedChild" : "";
-  const labelClass = !label ? "no-label" : "";
-  const labelText =
-    label && styleAsOptional ? labelTextWithOptional(label) : label;
+  const errorMessage = formErrorState?.[inputName]?.message;
+  const parsedHint = textbox.helperText && parseCustomHtml(textbox.helperText);
+  const labelText = textbox.label
+    ? labelTextWithOptional(textbox.label)
+    : textbox.label;
 
   return (
-    <Box sx={sxOverride} className={`${nestedChildClasses} ${labelClass}`}>
+    <Box>
       <CmsdsTextField
-        id={name}
-        name={name}
+        id={inputName}
+        name={inputName}
         label={labelText || ""}
         hint={parsedHint}
-        placeholder={placeholder}
-        errorMessage={errorMessage}
+        onChange={(e) => onChangeHandler(e)}
+        onBlur={(e) => onBlurHandler(e)}
         value={displayValue}
+        errorMessage={errorMessage}
         {...props}
       />
     </Box>
   );
 };
-
-interface Props {
-  name: string;
-  label?: string;
-  hint?: CustomHtmlElement[];
-  placeholder?: string;
-  sxOverride?: AnyObject;
-  nested?: boolean;
-  validateOnRender?: boolean;
-  styleAsOptional?: boolean;
-  clear?: boolean;
-  [key: string]: any;
-}
