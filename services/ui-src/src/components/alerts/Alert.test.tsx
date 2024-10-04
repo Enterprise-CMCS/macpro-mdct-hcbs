0801297123b5368ebd9591aeda4ffe87b060b1d5
Rocio De Santiago <rociodesantiago@CoformaSantiago.lan>
2024-10-03 19:14:14 -0500

    cover Alert.tsx line 34

--- services/ui-src/src/components/alerts/Alert.test.tsx
+++ services/ui-src/src/components/alerts/Alert.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import { Alert } from "components";
+import { AlertTypes } from "types";
 import { testA11y } from "utils/testing/commonTests";
 
 const alertComponent = (
@@ -30,4 +31,64 @@ describe("<Alert />", () => {
   });
 
   testA11y(alertComponent);
+});
+
+describe("Alert icon", () => {
+  const alertIcon = "test-file-stub";
+  const customIcon = "customIcon.png";
+  const title = "Test Alert";
+  const description = "This is a test description";
+
+  const sx = {
+    icon: { width: "100px" },
+    contentBox: {},
+    descriptionText: {},
+    linkText: {},
+  };
+
+  test("renders custom icon when icon prop is provided", () => {
+    render(
+      <Alert
+        status={AlertTypes.INFO}
+        title={title}
+        description={description}
+        icon={customIcon}
+        showIcon={true}
+        sx={sx}
+      />
+    );
+
+    const imgElement = screen.getByAltText("Alert");
+    expect(imgElement).toHaveAttribute("src", customIcon);
+  });
+
+  test("renders fallback alertIcon when icon prop is not provided", () => {
+    render(
+      <Alert
+        status={AlertTypes.INFO}
+        title={title}
+        description={description}
+        showIcon={true}
+        sx={sx}
+      />
+    );
+
+    const imgElement = screen.getByAltText("Alert");
+    expect(imgElement).toHaveAttribute("src", alertIcon);
+  });
+
+  it("does not render the icon when showIcon is false", () => {
+    render(
+      <Alert
+        status={AlertTypes.INFO}
+        title={title}
+        description={description}
+        showIcon={false}
+        sx={sx}
+      />
+    );
+
+    const imgElement = screen.queryByAltText("Alert");
+    expect(imgElement).not.toBeInTheDocument();
+  });
 });
