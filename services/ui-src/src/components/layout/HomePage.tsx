7daac07fc7403c41b08d8aff09ead48e6b17d712
Rocio De Santiago <rociodesantiago@CoformaSantiago.lan>
2024-10-22 18:39:07 -0500

    a rough first draft - just HTML

--- services/ui-src/src/components/layout/HomePage.tsx
+++ services/ui-src/src/components/layout/HomePage.tsx
@@ -1,28 +1,36 @@
 import { Box, Heading, Link, Text } from "@chakra-ui/react";
-import { PageTemplate, TemplateCard } from "components";
+import { AdminDashSelector, PageTemplate, TemplateCard } from "components";
+import { useStore } from "utils";
 import verbiage from "verbiage/pages/home";
 
 export const HomePage = () => {
   const { intro, cards } = verbiage;
+  const { userIsEndUser } = useStore().user ?? {};
 
   return (
     <PageTemplate>
-      <>
-        <Box>
-          <Heading as="h1" variant="h1">
-            {intro.header}
-          </Heading>
-          <Text>
-            {intro.body.preLinkText}
-            <Link href={intro.body.linkLocation} isExternal>
-              {intro.body.linkText}
-            </Link>
-            {intro.body.postLinkText}
-          </Text>
-          <Text></Text>
-        </Box>
-        <TemplateCard templateName="QM" verbiage={cards.QM} />
-      </>
+      {/* show standard view to state users */}
+      {userIsEndUser ? (
+        <>
+          <Box>
+            <Heading as="h1" variant="h1">
+              {intro.header}
+            </Heading>
+            <Text>
+              {intro.body.preLinkText}
+              <Link href={intro.body.linkLocation} isExternal>
+                {intro.body.linkText}
+              </Link>
+              {intro.body.postLinkText}
+            </Text>
+            <Text></Text>
+          </Box>
+          <TemplateCard templateName="QM" verbiage={cards.QM} />
+        </>
+      ) : (
+        // show read-only view to non-state users
+        <AdminDashSelector verbiage={verbiage.readOnly} />
+      )}
     </PageTemplate>
   );
 };
---
+++ services/ui-src/src/forms/adminDashSelector/adminDashSelector.ts
@@ -0,0 +1,52 @@
+import { States } from "../../constants";
+import { DropdownOptions } from "types";
+
+// create dropdown options
+const dropdownOptions: DropdownOptions[] = Object.keys(States).map((value) => {
+  return {
+    label: States[value as keyof typeof States],
+    value,
+  };
+});
+
+const reportChoices = [
+  {
+    id: "QM",
+    label: "Quality Measures (QM)",
+  },
+  {
+    id: "TBD",
+    label: "To be determined (TBD)",
+  },
+];
+
+export default {
+  id: "adminDashSelector",
+  editableByAdmins: true,
+  options: {
+    mode: "onChange",
+  },
+  fields: [
+    {
+      id: "state",
+      type: "dropdown",
+      validation: "dropdown",
+      props: {
+        hint: "Select state to view reports:",
+        options: dropdownOptions,
+        ariaLabel:
+          "List of states, including District of Columbia and Puerto Rico",
+      },
+    },
+    {
+      id: "report",
+      type: "radio",
+      validation: "radio",
+      props: {
+        hint: "Select a report:",
+        choices: reportChoices,
+        ariaLabel: "Choices of report type",
+      },
+    },
+  ],
+};
