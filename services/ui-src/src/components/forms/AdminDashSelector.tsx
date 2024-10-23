5db9b2047cf3c9142f20f32d484bc526649e1354
Rocio De Santiago <rociodesantiago@CoformaSantiago.lan>
2024-10-23 14:50:54 -0500

    Refactor design and include logic for state and report

--- services/ui-src/src/components/forms/AdminDashSelector.tsx
+++ services/ui-src/src/components/forms/AdminDashSelector.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Box, Button, Flex, Heading } from "@chakra-ui/react";
-import { AnyObject } from "types";
-import formJson from "forms/adminDashSelector/adminDashSelector";
+import { Dropdown, ChoiceList } from "@cmsgov/design-system";
+import { AnyObject, DropdownOptions } from "types";
+import { States } from "../../constants";
 
 interface FormProps {
   verbiage: AnyObject;
@@ -21,9 +22,42 @@ export const AdminDashSelector = ({ verbiage }: FormProps) => {
     setSelectedReport(event.target.value);
   };
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    navigate("dashboard");
+  const handleSubmit = () => {
+    navigate(`report/${selectedReport}/${selectedState}`);
+  };
+
+  const reportChoices = [
+    {
+      value: "QM",
+      label: "Quality Measures (QM)",
+    },
+    {
+      value: "TBD",
+      label: "To be determined (TBD)",
+    },
+  ];
+
+  const buildStates = () => {
+    const dropdownStates: DropdownOptions[] = Object.keys(States).map(
+      (value) => {
+        return {
+          label: States[value as keyof typeof States],
+          value,
+        };
+      }
+    );
+
+    return (
+      <Dropdown
+        name="state"
+        id="state"
+        label="Select state to view reports:"
+        ariaLabel="List of states, including District of Columbia and Puerto Rico"
+        options={dropdownStates}
+        onChange={handleStateChange}
+        value={selectedState}
+      />
+    );
   };
 
   return (
@@ -33,68 +67,20 @@ export const AdminDashSelector = ({ verbiage }: FormProps) => {
       </Heading>
 
       <form onSubmit={handleSubmit}>
-        {formJson.fields.map((field) => {
-          if (field.type === "dropdown") {
-            return (
-              <div key={field.id}>
-                <br />
-                <label htmlFor={field.id}>{field.props.hint}</label>
-                <br />
-                <select
-                  id={field.id}
-                  value={selectedState}
-                  onChange={handleStateChange}
-                  aria-label={field.props.ariaLabel}
-                  required
-                >
-                  <option value="" disabled>
-                    Select a state
-                  </option>
-                  {field.props.options?.map(
-                    (option: { label: string; value: string }) => (
-                      <option key={option.value} value={option.value}>
-                        {option.label}
-                      </option>
-                    )
-                  )}
-                </select>
-              </div>
-            );
-          }
-          if (field.type === "radio") {
-            return (
-              <div key={field.id}>
-                <br />
-                <p>{field.props.hint}</p>
-                {field.props.choices?.map(
-                  (choice: { id: string; label: string }) => (
-                    <div key={choice.id}>
-                      <input
-                        type="radio"
-                        id={choice.id}
-                        name="report"
-                        value={choice.id}
-                        checked={selectedReport === choice.id}
-                        onChange={handleReportChange}
-                        aria-label={field.props.ariaLabel}
-                        required
-                      />
-                      <label htmlFor={choice.id}>{choice.label}</label>
-                    </div>
-                  )
-                )}
-              </div>
-            );
-          }
-          return null;
-        })}
-
-        <br />
+        {buildStates()}
+        <ChoiceList
+          name="radio"
+          type="radio"
+          label="Select a report:"
+          choices={reportChoices}
+          onChange={handleReportChange}
+        />
         <Flex sx={sx.navigationButton}>
           <Button
             type="submit"
-            form={formJson.id}
-            onClick={() => navigate("dashboard")}
+            form="adminDashSelector"
+            onClick={() => handleSubmit()}
+            disabled={!selectedState || !selectedReport}
           >
             {verbiage.buttonLabel}
           </Button>
