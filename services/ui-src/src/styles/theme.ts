6d2aa4f0335b1c1b678bd61ce2a5ba4973bbc382
ailZhou <aileen.zhou@coforma.io>
2024-10-25 12:18:18 -0400

    fix merge issue

--- services/ui-src/src/styles/theme.ts
+++ services/ui-src/src/styles/theme.ts
@@ -330,12 +330,6 @@ const theme = extendTheme({
             fontSize: "2xl",
           },
         },
-        sidebar: {
-          fontSize: "21px",
-          fontWeight: "700",
-          padding: "2rem",
-          margin: "0",
-        },
         subHeader: {
           fontSize: "21px",
           fontWeight: "700",
@@ -351,11 +345,6 @@ const theme = extendTheme({
           fontWeight: "700",
           padding: "2rem",
           margin: "0",
-        },
-        login: {
-          my: "6rem",
-          textAlign: "center",
-          width: "100%",
         },
         login: {
           my: "6rem",
