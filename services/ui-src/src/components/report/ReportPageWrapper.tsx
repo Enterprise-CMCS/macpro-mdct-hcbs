a63b6dcfbe17cc5376357fb79c86faf22df51a8d
Rocio De Santiago <rociodesantiago@CoformaSantiago.lan>
2024-10-29 14:41:02 -0500

    add pra disclosure statement to bottom of general info page only

--- services/ui-src/src/components/report/ReportPageWrapper.tsx
+++ services/ui-src/src/components/report/ReportPageWrapper.tsx
@@ -10,6 +10,8 @@ import { FormProvider, useForm } from "react-hook-form";
 import { FormPageTemplate } from "types/report";
 
 export const ReportPageWrapper = () => {
+  const praStatement =
+    "Under the Privacy Act of 1974 any personally identifying information obtained will be kept private to the extent of the law. According to the Paperwork Reduction Act of 1995, no persons are required to respond to a collection of information unless it displays a valid OMB control number. The valid OMB control number for this information collection is 0938-1053. The time required to complete this information collection is estimated to average 2.5 hours per response, including the time to review instructions, search existing data resources, gather the data needed, and complete and review the information collection. If you have comments concerning the accuracy of the time estimate(s) or suggestions for improving this form, please write to: CMS, 7500 Security Boulevard, Attn: PRA Reports Clearance Officer, Mail Stop C4-26-05, Baltimore, Maryland 21244-1850";
   const {
     report,
     pageMap,
@@ -93,7 +95,7 @@ export const ReportPageWrapper = () => {
           </Box>
           {!currentPage.hideNavButtons && parentPage && (
             <>
-              <Divider></Divider>
+              {parentPage.index == 0 && <Divider></Divider>}
               <Flex width="100%">
                 {parentPage.index > 0 && (
                   <Button
@@ -103,15 +105,35 @@ export const ReportPageWrapper = () => {
                     Previous
                   </Button>
                 )}
-                {parentPage.index < parentPage.childPageIds.length - 1 && (
+                {parentPage.index < parentPage.childPageIds.length - 1 &&
+                  parentPage.index !== 0 && (
+                    <Button
+                      onClick={() => SetPageIndex(parentPage.index + 1)}
+                      marginLeft="auto"
+                    >
+                      Continue
+                    </Button>
+                  )}
+                {parentPage.index == 0 && (
                   <Button
                     onClick={() => SetPageIndex(parentPage.index + 1)}
                     marginLeft="auto"
+                    justifyContent={"flex-end"}
                   >
                     Continue
                   </Button>
                 )}
               </Flex>
+              <Box flex="auto" width="97.5%">
+                {parentPage.index == 0 && (
+                  <>
+                    <h4>
+                      <b>PRA Disclosure Statement</b>
+                    </h4>
+                    <p>{praStatement}</p>
+                  </>
+                )}
+              </Box>
             </>
           )}
         </VStack>
