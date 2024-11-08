2f2aa880032f02bedf436f167dcea83ca8dc1923
ailZhou <aileen.zhou@coforma.io>
2024-11-08 14:25:51 -0500

    just deploy please

--- services/ui-src/src/components/export/ExportedReportWrapper.test.tsx
+++ services/ui-src/src/components/export/ExportedReportWrapper.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import { ExportedReportWrapper } from "./ExportedReportWrapper";
 import { FormPageTemplate, PageElement, PageType } from "types";
 
