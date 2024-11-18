85b78c8a221f39ec8e83888d5221ca11201a7493
ailZhou <aileen.zhou@coforma.io>
2024-11-18 16:22:50 -0500

    Update Banner.test.tsx

--- services/ui-src/src/components/banners/Banner.test.tsx
+++ services/ui-src/src/components/banners/Banner.test.tsx
@@ -1,19 +1,19 @@
 import { render, screen } from "@testing-library/react";
-import {Banner} from "./Banner";
+import { Banner } from "./Banner";
 
 const bannerData = {
-  title: "mock-title", 
-  description: "mock-description"
-}
+  title: "mock-title",
+  description: "mock-description",
+};
 
 describe("Test Banner", () => {
   it("Test Banner with no data", () => {
-    const {container} = render(<Banner bannerData={undefined}></Banner>)
+    const { container } = render(<Banner bannerData={undefined}></Banner>);
     expect(container.childElementCount).toEqual(0);
-  })
+  });
   it("Test Banner with data", () => {
-    render(<Banner bannerData={bannerData}></Banner>)
+    render(<Banner bannerData={bannerData}></Banner>);
     expect(screen.getByText(bannerData.title)).toBeInTheDocument();
     expect(screen.getByText(bannerData.description)).toBeInTheDocument();
-  })
-})
+  });
+});
