e5288afc98a6e41a74e0067c7c20e036bfe24a56
ailZhou <aileen.zhou@coforma.io>
2024-11-25 14:15:53 -0500

    add banner to the homepage

--- services/ui-src/src/components/layout/HomePage.tsx
+++ services/ui-src/src/components/layout/HomePage.tsx
@@ -1,36 +1,61 @@
-import { Box, Heading, Link, Text } from "@chakra-ui/react";
-import { AdminDashSelector, PageTemplate, TemplateCard } from "components";
-import { useStore } from "utils";
+import { Box, Collapse, Heading, Link, Text } from "@chakra-ui/react";
+import {
+  AdminDashSelector,
+  Banner,
+  PageTemplate,
+  TemplateCard,
+} from "components";
+import { useEffect } from "react";
+import { checkDateRangeStatus, useStore } from "utils";
 import verbiage from "verbiage/pages/home";
 
 export const HomePage = () => {
+  const { bannerData, bannerActive, setBannerActive } = useStore();
   const { intro, cards } = verbiage;
   const { userIsEndUser } = useStore().user ?? {};
 
+  useEffect(() => {
+    let bannerActivity = false;
+    if (bannerData && bannerData.startDate && bannerData.endDate) {
+      bannerActivity = checkDateRangeStatus(
+        bannerData.startDate,
+        bannerData.endDate
+      );
+    }
+    setBannerActive(bannerActivity);
+  }, [bannerData]);
+
+  const showBanner = !!bannerData?.key && bannerActive;
+
   return (
-    <PageTemplate>
-      {/* show standard view to state users */}
-      {userIsEndUser ? (
-        <>
-          <Box>
-            <Heading as="h1" variant="h1">
-              {intro.header}
-            </Heading>
-            <Text>
-              {intro.body.preLinkText}
-              <Link href={intro.body.linkLocation} isExternal>
-                {intro.body.linkText}
-              </Link>
-              {intro.body.postLinkText}
-            </Text>
-            <Text></Text>
-          </Box>
-          <TemplateCard templateName="QM" verbiage={cards.QM} />
-        </>
-      ) : (
-        // show read-only view to non-state users
-        <AdminDashSelector verbiage={verbiage.readOnly} />
-      )}
-    </PageTemplate>
+    <>
+      <Collapse in={showBanner}>
+        <Banner bannerData={bannerData} />
+      </Collapse>
+      <PageTemplate>
+        {/* show standard view to state users */}
+        {userIsEndUser ? (
+          <>
+            <Box>
+              <Heading as="h1" variant="h1">
+                {intro.header}
+              </Heading>
+              <Text>
+                {intro.body.preLinkText}
+                <Link href={intro.body.linkLocation} isExternal>
+                  {intro.body.linkText}
+                </Link>
+                {intro.body.postLinkText}
+              </Text>
+              <Text></Text>
+            </Box>
+            <TemplateCard templateName="QM" verbiage={cards.QM} />
+          </>
+        ) : (
+          // show read-only view to non-state users
+          <AdminDashSelector verbiage={verbiage.readOnly} />
+        )}
+      </PageTemplate>
+    </>
   );
 };
