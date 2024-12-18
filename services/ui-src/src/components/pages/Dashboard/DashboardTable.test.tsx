e9c205ec0836a0418097e0a912de6a3d5e1f3e85
Angela Cooney <angela.cooney11@gmail.com>
2024-12-18 11:36:21 -0500

    CMDCT-4185: fix test

--- services/ui-src/src/components/pages/Dashboard/DashboardTable.test.tsx
+++ services/ui-src/src/components/pages/Dashboard/DashboardTable.test.tsx
@@ -34,7 +34,7 @@ describe("Dashboard table with state user", () => {
   it("should render report name and edit button in table", async () => {
     render(dashboardTableComponent);
     expect(screen.getByText("report 1")).toBeInTheDocument();
-    expect(screen.getByAltText("Edit Report")).toBeInTheDocument();
+    expect(screen.getByAltText("Edit Report Name")).toBeInTheDocument();
   });
 });
 
@@ -43,6 +43,6 @@ describe("Dashboard table with Read only user", () => {
   it("should not render the edit button when read only", async () => {
     render(readOnlyDashboardTableComponent);
     expect(screen.getByText("report 1")).toBeInTheDocument();
-    expect(screen.queryByAltText("Edit Report")).not.toBeInTheDocument();
+    expect(screen.queryByAltText("Edit Report Name")).not.toBeInTheDocument();
   });
 });
