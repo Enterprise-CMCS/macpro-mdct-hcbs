be1e0eb3cd249bd19a67cbce0ed397f8e1786f18
Angela Cooney <angela.cooney11@gmail.com>
2024-12-31 15:20:26 -0500

    Removing unnecessary code and writing tests

--- services/app-api/utils/tests/validation.test.ts
+++ services/app-api/utils/tests/validation.test.ts
@@ -1,101 +1,43 @@
-import {
-  filterValidationSchema,
-  mapValidationTypesToSchema,
-} from "../validation";
-import * as schema from "../schemaMap";
+import { error } from "../constants";
+import { validateData } from "../validation";
 
-const mockStandardValidationType = {
-  key: "text",
-};
+import { number, object, string } from "yup";
 
-const mockNestedValidationType = {
-  key: {
-    type: "text",
-    nested: true,
-    parentFieldName: "mock-parent-field-name",
-    parentOptionId: "mock-parent-option-name",
-  },
-};
-
-const mockDependentValidationType = {
-  key: {
-    type: "endDate",
-    dependentFieldName: "mock-dependent-field-name",
-  },
-};
-
-const mockNestedDependentValidationType = {
-  key: {
-    type: "endDate",
-    dependentFieldName: "mock-dependent-field-name",
-    nested: true,
-    parentFieldName: "mock-parent-field-name",
-    parentOptionId: "mock-parent-option-name",
-  },
-};
-
-describe("Test mapValidationTypesToSchema", () => {
-  it("Returns standard validation schema if passed standard validation type", () => {
-    const result = mapValidationTypesToSchema(mockStandardValidationType);
-    expect(JSON.stringify(result)).toEqual(
-      JSON.stringify({ key: schema.text() })
-    );
-  });
-
-  it("Returns nested validation schema if passed nested validation type", () => {
-    const result = mapValidationTypesToSchema(mockNestedValidationType);
-    expect(JSON.stringify(result)).toEqual(
-      JSON.stringify({
-        key: schema.nested(
-          () => schema.text(),
-          "mock-parent-field-name",
-          "mock-parent-option-name"
-        ),
-      })
-    );
-  });
-
-  it("Returns dependent validation schema if passed dependent validation type", () => {
-    const result = mapValidationTypesToSchema(mockDependentValidationType);
-    expect(JSON.stringify(result)).toEqual(
-      JSON.stringify({
-        key: schema.endDate("mock-dependent-field-name"),
-      })
-    );
-  });
-
-  it("Returns nested dependent validation schema if passed nested dependent validation type", () => {
-    const result = mapValidationTypesToSchema(
-      mockNestedDependentValidationType
-    );
-    expect(JSON.stringify(result)).toEqual(
-      JSON.stringify({
-        key: schema.nested(
-          () => schema.endDate("mock-dependent-field-name"),
-          "mock-parent-field-name",
-          "mock-parent-option-name"
-        ),
-      })
-    );
-  });
+const validationSchema = object().shape({
+  key: string().required(),
+  title: string().required(),
+  description: string().required(),
+  link: string().url().notRequired(),
+  startDate: number().notRequired(),
+  endDate: number().notRequired(),
 });
 
-const mockValidationObject = {
-  "mock-field-1": "text",
-  "mock-field-2": "email",
-  "mock-field-3": "number",
-};
-const mockDataObject = {
-  "mock-field-1": undefined,
-  "mock-field-3": undefined,
+const validObject = {
+  key: "1023",
+  title: "this is a title",
+  description: "this is a description",
+  link: "https://www.google.com",
+  startDate: 11022933,
+  endDate: 103444405,
 };
 
-describe("Test filterValidationSchema", () => {
-  it("Filters out validation objects for which there is no field data being passed", () => {
-    const result = filterValidationSchema(mockValidationObject, mockDataObject);
-    expect(result).toEqual({
-      "mock-field-1": "text",
-      "mock-field-3": "number",
-    });
+const invalidObject = {
+  // missing key
+  title: "this is a title",
+  description: "this is a description",
+  link: "https://www.google.com",
+  startDate: 11022933,
+  endDate: 103444405,
+};
+
+describe("Test validateData function", () => {
+  it("successfully validates a valid object", async () => {
+    const validatedData = await validateData(validationSchema, validObject);
+    expect(validatedData).toEqual(validObject);
+  });
+  it("throws an error when validating an invalid object", () => {
+    expect(async () => {
+      await validateData(validationSchema, invalidObject);
+    }).rejects.toThrow(error.INVALID_DATA);
   });
 });
--- services/app-api/utils/validation.ts
+++ services/app-api/utils/validation.ts
@@ -1,6 +1,5 @@
 import { AnyObject } from "../types/types";
 import { error } from "./constants";
-import { endDate, nested, schemaMap } from "./schemaMap";
 
 // compare payload data against validation schema
 export const validateData = async (
@@ -16,66 +15,5 @@ export const validateData = async (
     });
   } catch {
     throw new Error(error.INVALID_DATA);
-  }
-};
-
-// filter field validation to just what's needed for the passed fields
-export const filterValidationSchema = (
-  validationObject: AnyObject,
-  data: AnyObject
-): AnyObject => {
-  const validationEntries = Object.entries(validationObject);
-  const dataKeys = Object.keys(data);
-  const filteredEntries = validationEntries.filter(
-    (entry: [string, string | AnyObject]) => {
-      const [entryKey] = entry;
-      return dataKeys.includes(entryKey);
-    }
-  );
-  return Object.fromEntries(filteredEntries);
-};
-
-// map field validation types to validation schema
-export const mapValidationTypesToSchema = (fieldValidationTypes: AnyObject) => {
-  let validationSchema: AnyObject = {};
-  // for each field to be validated,
-  Object.entries(fieldValidationTypes).forEach(
-    (fieldValidationType: [string, string | AnyObject]) => {
-      const [key, fieldValidation] = fieldValidationType;
-      // if standard validation type, set corresponding schema from map
-      if (typeof fieldValidation === "string") {
-        const correspondingSchema = schemaMap[fieldValidation];
-        if (correspondingSchema) {
-          validationSchema[key] = correspondingSchema;
-        }
-      }
-      // else if nested validation type, make and set nested schema
-      else if (fieldValidation.nested) {
-        validationSchema[key] = makeNestedFieldSchema(fieldValidation);
-        // else if not nested, make and set other dependent field types
-      } else if (fieldValidation.type === "endDate") {
-        validationSchema[key] = makeEndDateFieldSchema(fieldValidation);
-      }
-    }
-  );
-  return validationSchema;
-};
-
-export const makeEndDateFieldSchema = (fieldValidationObject: AnyObject) => {
-  const { dependentFieldName } = fieldValidationObject;
-  return endDate(dependentFieldName);
-};
-
-export const makeNestedFieldSchema = (fieldValidationObject: AnyObject) => {
-  const { type, parentFieldName, parentOptionId } = fieldValidationObject;
-  if (fieldValidationObject.type === "endDate") {
-    return nested(
-      () => makeEndDateFieldSchema(fieldValidationObject),
-      parentFieldName,
-      parentOptionId
-    );
-  } else {
-    const fieldValidationSchema = schemaMap[type];
-    return nested(() => fieldValidationSchema, parentFieldName, parentOptionId);
   }
 };
