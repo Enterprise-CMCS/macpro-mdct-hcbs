5a82b26364f9f3f012e69ab7681d61e50a60cb65
ajaitasaini <ajaita.saini@coforma.io>
2025-01-22 10:52:20 -0500

    [HCBS] Create Placeholder for TA and CI Reports (#87)

--- services/ui-src/src/components/layout/HomePage.tsx
+++ services/ui-src/src/components/layout/HomePage.tsx
@@ -8,11 +8,15 @@ import {
 import { useEffect } from "react";
 import { checkDateRangeStatus, useStore } from "utils";
 import verbiage from "verbiage/pages/home";
+import { useFlags } from "launchdarkly-react-client-sdk";
 
 export const HomePage = () => {
   const { bannerData, bannerActive, setBannerActive } = useStore();
   const { intro, cards } = verbiage;
   const { userIsEndUser } = useStore().user ?? {};
+
+  const isTAReportActive = useFlags()?.isTaReportActive;
+  const isCIReportActive = useFlags()?.isCiReportActive;
 
   useEffect(() => {
     let bannerActivity = false;
@@ -52,6 +56,12 @@ export const HomePage = () => {
               <Text></Text>
             </Box>
             <TemplateCard templateName="QMS" verbiage={cards.QMS} />
+            {isTAReportActive && (
+              <TemplateCard templateName="TA" verbiage={cards.TA} />
+            )}
+            {isCIReportActive && (
+              <TemplateCard templateName="CI" verbiage={cards.CI} />
+            )}
           </>
         ) : (
           // show read-only view to non-state users
