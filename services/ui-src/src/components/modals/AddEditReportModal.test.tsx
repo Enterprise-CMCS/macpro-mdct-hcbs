6dd3009438fce066604315ae9b730c543b85d2fa
ailZhou <aileen.zhou@coforma.io>
2025-02-24 11:35:47 -0500

    better unit test

--- services/ui-src/src/components/modals/AddEditReportModal.test.tsx
+++ services/ui-src/src/components/modals/AddEditReportModal.test.tsx
@@ -14,6 +14,11 @@ const mockReportHandler = jest.fn();
 jest.mock("utils/state/useStore");
 const mockedUseStore = useStore as jest.MockedFunction<typeof useStore>;
 
+jest.mock("utils/api/requestMethods/report", () => ({
+  putReport: jest.fn(),
+  createReport: jest.fn(),
+}));
+
 const addModalComponent = (
   <RouterWrappedComponent>
     <AddEditReportModal
@@ -132,6 +137,29 @@ describe("Test dropdown for year", () => {
     }) as HTMLSelectElement;
     userEvent.selectOptions(dropdown, "2026");
     expect(dropdown.value).toBe("2026");
+  });
+});
+
+describe("Test submit", () => {
+  beforeEach(() => {
+    render(addModalComponent);
+  });
+
+  it("Simulate submitting modal", async () => {
+    const nameTextbox = screen.getByRole("textbox", {
+      name: "QMS report name Name this QMS report so you can easily refer to it. Consider using timeframe(s).",
+    });
+    await userEvent.type(nameTextbox, "mock-name");
+
+    const radioBtns = screen.getAllByLabelText("Yes");
+    for (const radio of radioBtns) {
+      await userEvent.click(radio);
+    }
+
+    const submitBtn = screen.getByText("Start new");
+    await userEvent.click(submitBtn);
+
+    expect(mockReportHandler).toHaveBeenCalled();
   });
 });
 
