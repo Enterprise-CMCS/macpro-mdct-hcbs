f603634995c62f30f1fc80dff1e74f9550842b58
BearHanded <bcm3223@gmail.com>
2025-02-25 13:49:24 -0500

    modal reset button

---
+++ services/ui-src/src/components/report/MeasureClearModal.tsx
@@ -0,0 +1,28 @@
+import { ModalBody, ModalFooter, Button } from "@chakra-ui/react";
+import React from "react";
+import { MeasurePageTemplate } from "types";
+
+export const MeasureClearModal = (
+  measure: MeasurePageTemplate,
+  onClose: Function,
+  onSubmit: Function
+): React.ReactFragment => {
+  const submitReset = () => {
+    onSubmit(measure.id);
+    onClose();
+  };
+  const body = `This action cannot be undone. It will clear all data that has been entered into the measure ${measure.id} and reset this measure's status to 'Not started'`;
+  return (
+    <React.Fragment>
+      <ModalBody>{body}</ModalBody>
+      <ModalFooter gap="4">
+        <Button colorScheme="blue" mr={3} onClick={() => submitReset()}>
+          Clear measure data
+        </Button>
+        <Button variant="link" onClick={() => onClose()}>
+          Cancel
+        </Button>
+      </ModalFooter>
+    </React.Fragment>
+  );
+};
