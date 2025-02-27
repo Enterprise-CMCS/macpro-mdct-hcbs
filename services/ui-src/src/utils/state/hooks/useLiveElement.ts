55e6cfa073fe8f4be69a9a2e0391329aa3a5a521
BearHanded <bcm3223@gmail.com>
2025-02-27 10:29:21 -0500

    add tests

--- services/ui-src/src/utils/state/hooks/useLiveElement.ts
+++ services/ui-src/src/utils/state/hooks/useLiveElement.ts
@@ -3,6 +3,16 @@ import { useWatch } from "react-hook-form";
 import { PageElement } from "types";
 import { useStore } from "utils";
 
+/**
+ * This hook gives the most up to date version of an element on the current page.
+ * This solves two problems:
+ *   - The react-hook-form hooks can init empty but they are always the most up to date after
+ *   - The store only updates (in most instances) on blur
+ * When we need elements to visually change immediately based on other elements, this bridges the divide
+ * Useful for instances like the QM table display, but not for everything
+ * @param elementId The id of the element on the current page to watch
+ * @returns A page element or undefined
+ */
 export const useLiveElement = (elementId: string) => {
   const [liveElement, setLiveElement] = useState<PageElement | undefined>();
   const { report, pageMap, currentPageId } = useStore();
@@ -14,9 +24,12 @@ export const useLiveElement = (elementId: string) => {
   const elementIndex = currentPage?.elements?.findIndex(
     (element) => element.id === elementId
   );
+
+  // Get elements from both sources
   const stateElement = currentPage?.elements[elementIndex];
   const hookFormElement = useWatch({ name: `elements.${elementIndex}` });
 
+  // Hook runs whenever either source updates, merge
   useEffect(() => {
     setLiveElement({
       ...stateElement,
