bcfcbc84e6ad1b390aa06fa96086d4e92170e104
ailZhou <aileen.zhou@coforma.io>
2025-04-04 09:22:42 -0400

    added page test

--- services/ui-src/src/components/report/Page.test.tsx
+++ services/ui-src/src/components/report/Page.test.tsx
@@ -8,6 +8,7 @@ import { render, screen } from "@testing-library/react";
 import { ElementType, PageElement } from "types/report";
 import { useStore } from "utils";
 import { Page } from "./Page";
+import { AlertTypes } from "types";
 
 jest.mock("react-router-dom", () => ({
   useNavigate: jest.fn(),
@@ -151,6 +152,12 @@ const elements: PageElement[] = [
   {
     type: ElementType.Divider,
     id: "",
+  },
+  {
+    type: ElementType.StatusAlert,
+    id: "",
+    text: "mock status",
+    status: AlertTypes.ERROR,
   },
 ];
 
