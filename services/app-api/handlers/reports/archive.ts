098a3ca808173261afe163ead584b0cfdec84420
BearHanded <bcm3223@gmail.com>
2025-04-04 14:05:54 -0400

    Archive working

---
+++ services/app-api/handlers/reports/archive.ts
@@ -0,0 +1,31 @@
+import { handler } from "../../libs/handler-lib";
+import { parseReportParameters } from "../../libs/param-lib";
+import { badRequest, forbidden, notFound, ok } from "../../libs/response-lib";
+import { getReport, putReport } from "../../storage/reports";
+import { canArchiveReport } from "../../utils/authorization";
+import { error } from "../../utils/constants";
+
+export const updateArchiveStatus = handler(
+  parseReportParameters,
+  async (request) => {
+    const { reportType, state, id } = request.parameters;
+    const user = request.user;
+
+    if (!canArchiveReport(user)) {
+      return forbidden(error.UNAUTHORIZED);
+    }
+
+    if (!request?.body) {
+      return badRequest("Invalid request");
+    }
+
+    const { archived } = request.body as { archived: boolean };
+    const report = await getReport(reportType, state, id);
+
+    if (!report) return notFound();
+    report.archived = archived;
+    await putReport(report);
+
+    return ok();
+  }
+);
