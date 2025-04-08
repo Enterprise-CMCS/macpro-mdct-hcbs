5b013eda28dc28bbf80c967d386ab4a11b3e3a10
Rocio De Santiago <rociodesantiago@CoformaSantiago.lan>
2025-04-08 14:09:38 -0500

    testssss

--- services/ui-src/src/utils/state/management/reportState.test.ts
+++ services/ui-src/src/utils/state/management/reportState.test.ts
@@ -267,14 +267,6 @@ const testReport: Report = {
       type: PageType.Measure,
       elements: [],
     },
-    [MeasureTemplateName["FFS-6"]]: {
-      id: "",
-      cmitId: "",
-      status: MeasureStatus.IN_PROGRESS,
-      title: "",
-      type: PageType.Measure,
-      elements: [],
-    },
     [MeasureTemplateName["MLTSS-6"]]: {
       id: "",
       title: "",
