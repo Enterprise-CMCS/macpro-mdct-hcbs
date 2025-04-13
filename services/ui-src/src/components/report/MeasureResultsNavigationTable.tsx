e13b27cb3f19a049fe1c7c7e6a4d240750e5fdd6
BearHanded <bcm3223@gmail.com>
2025-04-12 23:05:58 -0400

    fix test

--- services/ui-src/src/components/report/MeasureResultsNavigationTable.tsx
+++ services/ui-src/src/components/report/MeasureResultsNavigationTable.tsx
@@ -50,7 +50,7 @@ export const MeasureResultsNavigationTableElement = () => {
     if (disabled) return;
 
     //TO DO: add real code
-    if (measure.status !== PageStatus.COMPLETE)
+    if (measure?.status !== PageStatus.COMPLETE)
       return <Text variant="error">Select "Edit" to report progress.</Text>;
 
     return <></>;
