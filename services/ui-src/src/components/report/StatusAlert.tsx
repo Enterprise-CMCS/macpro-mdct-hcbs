dbc9f6080fafbc39a05e4d047126b165a1b17d09
ailZhou <aileen.zhou@coforma.io>
2025-04-15 14:12:39 -0400

    Update StatusAlert.tsx

--- services/ui-src/src/components/report/StatusAlert.tsx
+++ services/ui-src/src/components/report/StatusAlert.tsx
@@ -3,30 +3,31 @@ import { Alert } from "components/alerts/Alert";
 import { PageElementProps } from "./Elements";
 import { PageStatus, StatusAlertTemplate } from "types";
 import { inferredReportStatus } from "utils/state/reportLogic/completeness";
+import { useState } from "react";
 
 export const StatusAlert = (props: PageElementProps) => {
   const alert = props.element as StatusAlertTemplate;
   const { report, currentPageId } = useStore();
+  const [hideAlert, setHideAlert] = useState<boolean>(false);
 
   if (report && currentPageId) {
     switch (currentPageId) {
       case "review-submit":
         const childPages = report.pages[0].childPageIds?.slice(0, -1);
-
         const status = childPages?.every(
           (page) => inferredReportStatus(report, page) === PageStatus.COMPLETE
         );
 
-        if (status) return <></>;
+        setHideAlert(status!);
         break;
       default:
         const displayStatus = inferredReportStatus(report, currentPageId);
-        if (displayStatus != PageStatus.COMPLETE) {
-          return <></>;
-        }
+        setHideAlert(displayStatus != PageStatus.COMPLETE);
         break;
     }
   }
+
+  if (hideAlert) return <></>;
 
   return (
     <Alert
