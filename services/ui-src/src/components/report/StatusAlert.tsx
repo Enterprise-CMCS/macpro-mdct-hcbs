5d62a1f7df13c28fb95b5c90de659517cb15add1
BearHanded <bcm3223@gmail.com>
2025-04-17 14:50:25 -0400

    consolidate logic for alert

--- services/ui-src/src/components/report/StatusAlert.tsx
+++ services/ui-src/src/components/report/StatusAlert.tsx
@@ -1,27 +1,25 @@
 import { useStore } from "utils";
 import { Alert } from "components/alerts/Alert";
 import { PageElementProps } from "./Elements";
-import { PageStatus, StatusAlertTemplate } from "types";
-import { inferredReportStatus } from "utils/state/reportLogic/completeness";
+import { StatusAlertTemplate } from "types";
+import {
+  currentPageCompletableSelector,
+  submittableMetricsSelector,
+} from "utils/state/selectors";
 
 export const StatusAlert = (props: PageElementProps) => {
   const alert = props.element as StatusAlertTemplate;
   const { report, currentPageId } = useStore();
+  const submittableMetrics = useStore(submittableMetricsSelector);
+  const currentPageCompletable = useStore(currentPageCompletableSelector);
 
   if (!report) return <></>;
 
-  if (currentPageId === "review-submit") {
-    const childPages = report.pages[0].childPageIds?.slice(0, -1);
-    const status = childPages?.every(
-      (page) => inferredReportStatus(report, page) === PageStatus.COMPLETE
-    );
-
-    //TO DO: add code for handling optional page
-
-    if (status) return <></>;
-  } else {
-    const displayStatus = inferredReportStatus(report, currentPageId!);
-    if (displayStatus != PageStatus.COMPLETE) return <></>;
+  const isReviewPage = currentPageId === "review-submit";
+  if (isReviewPage && submittableMetrics?.submittable) {
+    return <></>;
+  } else if (!isReviewPage && currentPageCompletable) {
+    return <></>;
   }
 
   return (
--- services/ui-src/src/utils/state/selectors.ts
+++ services/ui-src/src/utils/state/selectors.ts
@@ -1,5 +1,13 @@
-import { HcbsReportState } from "types";
-import { pageIsCompletable } from "./reportLogic/completeness";
+import {
+  HcbsReportState,
+  PageStatus,
+  ParentPageTemplate,
+  ReportStatus,
+} from "types";
+import {
+  inferredReportStatus,
+  pageIsCompletable,
+} from "./reportLogic/completeness";
 
 export const currentPageSelector = (state: HcbsReportState) => {
   const { report, pageMap, currentPageId } = state;
@@ -14,4 +22,40 @@ export const currentPageSelector = (state: HcbsReportState) => {
 export const currentPageCompletableSelector = (state: HcbsReportState) => {
   if (!state.report || !state.currentPageId) return false;
   return pageIsCompletable(state.report, state.currentPageId);
+};
+
+export const submittableMetricsSelector = (state: HcbsReportState) => {
+  const { report, pageMap } = state;
+  if (!report || !pageMap) {
+    return null;
+  }
+
+  const childPages = (report.pages[pageMap.get("root")!] as ParentPageTemplate)
+    .childPageIds;
+  const sections = childPages.slice(0, -1).map((id) => {
+    const pageIdx = pageMap.get(id);
+    if (!pageIdx) return null;
+    const section = report.pages[pageIdx] as ParentPageTemplate;
+    let displayStatus = inferredReportStatus(report, section.id);
+    let submittable = displayStatus === PageStatus.COMPLETE;
+
+    if (section.id === "optional-measure-result") {
+      submittable = displayStatus !== PageStatus.IN_PROGRESS;
+      if (displayStatus === PageStatus.NOT_STARTED) displayStatus = undefined;
+    }
+
+    return {
+      section: section,
+      displayStatus: displayStatus,
+      submittable: submittable,
+    };
+  });
+
+  const allPagesSubmittable = sections.every(
+    (sectionInfo) => !!sectionInfo?.submittable
+  );
+  const submittable =
+    report.status !== ReportStatus.SUBMITTED && allPagesSubmittable;
+
+  return { sections: sections, submittable: submittable };
 };
