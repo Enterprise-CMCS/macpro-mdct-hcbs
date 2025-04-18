83ccf03f5ba3717462c6113a0160d63b5592aeb0
ailZhou <aileen.zhou@coforma.io>
2025-04-18 15:02:18 -0400

    fix link issue

--- services/ui-src/src/components/report/StatusAlert.tsx
+++ services/ui-src/src/components/report/StatusAlert.tsx
@@ -1,13 +1,17 @@
-import { useStore } from "utils";
+import { useNavigate, useParams } from "react-router-dom";
+import { measurePrevPage, useStore } from "utils";
 import { Alert } from "components/alerts/Alert";
 import { PageElementProps } from "./Elements";
-import { PageStatus, StatusAlertTemplate } from "types";
+import { ButtonLinkTemplate, PageStatus, StatusAlertTemplate } from "types";
 import { submittableMetricsSelector } from "utils/state/selectors";
 import { inferredReportStatus } from "utils/state/reportLogic/completeness";
+import { Link } from "@chakra-ui/react";
 
 export const StatusAlert = (props: PageElementProps) => {
-  const alert = props.element as StatusAlertTemplate;
+  const navigate = useNavigate();
   const { report, currentPageId } = useStore();
+  const { reportType, state, reportId, pageId } = useParams();
+  const alert = props.element as StatusAlertTemplate;
   const submittableMetrics = useStore(submittableMetricsSelector);
 
   if (!report || !currentPageId) return <></>;
@@ -21,11 +25,25 @@ export const StatusAlert = (props: PageElementProps) => {
     return <></>;
   }
 
+  const page = report.pages.find((page) => page.id === pageId);
+  const returnElement = page?.elements?.find(
+    (element) => element.id === "return-button"
+  ) as ButtonLinkTemplate;
+  const pageTo = returnElement?.to
+    ? returnElement.to
+    : measurePrevPage(report, pageId!);
+  const textElements = alert.text.split("<a>Click here</a>");
+
+  const nav = () =>
+    navigate(`/report/${reportType}/${state}/${reportId}/${pageTo}`);
+
+  const link = <Link onClick={() => nav()}>Click here</Link>;
+
   return (
     <Alert
       status={alert.status}
       title={alert.title}
-      children={alert.text}
+      children={[textElements[0], link, textElements[1]]}
     ></Alert>
   );
 };
