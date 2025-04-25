2b1fe30fd3177f25c2b3cac6f765b99f9c93cfd3
Brendan Myers <6362494+BearHanded@users.noreply.github.com>
2025-04-25 13:09:47 -0400

    [CMDCT-4557] Content Update - Intro page (#222)

--- services/ui-src/src/components/cards/QmsIntroductionCard.tsx
+++ services/ui-src/src/components/cards/QmsIntroductionCard.tsx
@@ -10,21 +10,20 @@ import { ReportIntroCardActions } from "./ReportIntroCardActions";
  */
 export const QmsIntroductionCard = () => {
   return (
-    <ReportIntroCard title="HCBS Quality Measure Set">
-      The HCBS is ...
-      <Link
-        isExternal={true}
-        href="https://www.govinfo.gov/content/pkg/PLAW-109publ171/pdf/PLAW-109publ171.pdf"
-      >
-        6071(a)(1) of the Deficit Reduction Act (DRA)
-      </Link>
-      as "increasing the use of home and community-based, rather than
-      institutional, long-term care services."
+    <ReportIntroCard title="Quality Measure Set Report">
+      The HCBS Quality Measure Set supports ongoing efforts to safeguard the
+      health and welfare of HCBS participants by promoting quality monitoring
+      and accountability across state systems. The federally required
+      performance measures (PMs) in this set assess state performance in key
+      domains such as service planning, person-centeredness, health and welfare,
+      and community integration. These PMs are reported annually and help track
+      whether states are effectively implementing quality strategies and
+      delivering services that meet participant needs. The HCBS Quality Measure
+      Set PMs include both fee-for-service and managed care delivery systems.
       <ReportIntroCardActions reportType={ReportType.QMS} />
       <Accordion allowToggle={true}>
-        <AccordionItem label="When is the HCBS Quality Measure Set due?">
-          <p>The HCBS Quality Measure Set will be created and submitted ...</p>
-          <p>The HCBS Quality Measure Set deadlines are TBD ...</p>
+        <AccordionItem label="When is the Quality Measure Set Report due?">
+          [CMS to provide]
         </AccordionItem>
       </Accordion>
     </ReportIntroCard>
