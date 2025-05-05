2b86a3ccc198d8f29a2f69cf8b9e088cab18f33a
benmartin-coforma <Ben.Martin@coforma.io>
2025-05-05 10:53:21 -0600

    Remove unneeded type annotation

--- services/app-api/handlers/reports/update.ts
+++ services/app-api/handlers/reports/update.ts
@@ -3,7 +3,7 @@ import { handler } from "../../libs/handler-lib";
 import { parseReportParameters } from "../../libs/param-lib";
 import { badRequest, forbidden, ok } from "../../libs/response-lib";
 import { putReport } from "../../storage/reports";
-import { Report, ReportStatus } from "../../types/reports";
+import { ReportStatus } from "../../types/reports";
 import { canWriteState } from "../../utils/authorization";
 import { error } from "../../utils/constants";
 import { validateReportPayload } from "../../utils/reportValidation";
@@ -20,7 +20,7 @@ export const updateReport = handler(parseReportParameters, async (request) => {
     return badRequest("Invalid request");
   }
 
-  let report: Report;
+  let report;
   try {
     report = await validateReportPayload(request.body);
   } catch (err) {
