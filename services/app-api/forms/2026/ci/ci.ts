e61fade412b219d4c7ab3aae996e79f489ce00c8
ajaitasaini <ajaita.saini@coforma.io>
2025-07-22 13:23:22 -0400

    [HCBS] Critical Incident - Review and Submit Page (#313)

--- services/app-api/forms/2026/ci/ci.ts
+++ services/app-api/forms/2026/ci/ci.ts
@@ -167,7 +167,7 @@ export const ciReportTemplate: ReportBase = {
           id: "review-alert",
           status: "error",
           title: "Your form is not ready for submission",
-          text: "Some sections of the CICM Report have errors or are missing required responses. Ensure all required and in-progress measures are completed with valid responses before submitting. If an optional measure is showing as ‘in-progress’ and you do not want to complete that measure, go into the measure and clear the data to reset it.",
+          text: "Some sections of the Critical Incident Report have errors or are missing required responses. Ensure all required fields are completed with valid responses before submitting.",
         },
         {
           type: ElementType.Header,
@@ -178,13 +178,13 @@ export const ciReportTemplate: ReportBase = {
           type: ElementType.Paragraph,
           id: "review-text",
           title: "Ready to submit?",
-          text: "Double check that everything in your CICM Report is accurate. To make edits to your report after submitting, contact your CMS HCBS Lead to unlock your report.",
+          text: 'Double check that everything in your CI Report is accurate. You will be able to make edits after submitting if you contact your CMS HCBS Lead to unlock your report while it is in "Submitted" status.',
         },
         {
           type: ElementType.Paragraph,
           id: "review-compliance",
           title: "Compliance review",
-          text: "Your CMS HCBS Lead will review your report and may unlock it for editing if corrections are needed.",
+          text: "Your CMS HCBS Lead will review your report and may contact you and unlock your report for editing if there are corrections to be made.",
         },
         {
           type: ElementType.StatusTable,
@@ -217,7 +217,7 @@ export const ciReportTemplate: ReportBase = {
           type: ElementType.Paragraph,
           weight: "bold",
           id: "submitted-what-happens",
-          text: "Email your CMS representative to inform them you submitted the CICM Report and it is ready for their review.",
+          text: "Email your CMS representative to inform them you submitted the CI Report and it is ready for their review.",
         },
       ],
     },
