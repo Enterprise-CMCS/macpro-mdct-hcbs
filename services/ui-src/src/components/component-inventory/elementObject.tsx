22be0a227c7bf2f0890dfdaacda0f947a40a4c9d
Rocio De Santiago <rociodesantiago@CoformaSantiago.lan>
2025-07-24 17:04:16 -0500

    add dropdown, radio, buttonlink kinda, date, measure subheader and number field

--- services/ui-src/src/components/component-inventory/elementObject.tsx
+++ services/ui-src/src/components/component-inventory/elementObject.tsx
@@ -1,14 +1,23 @@
-import { Accordion, Divider } from "@chakra-ui/react";
-import { TextField } from "components/fields/TextField";
-import { TextAreaField } from "components/fields/TextAreaField";
-import { AccordionItem } from "components";
+import { Accordion } from "@chakra-ui/react";
 import {
+  DateField,
+  DropdownField,
+  RadioField,
+  TextAreaField,
+  TextField,
+} from "components/fields";
+import { AccordionItem } from "components";
+
+import {
+  ButtonLinkElement,
+  DividerElement,
   HeaderElement,
   NestedHeadingElement,
   ParagraphElement,
   SubHeaderElement,
+  SubHeaderMeasureElement,
 } from "components/report/Elements";
-import { ElementType, HeaderIcon } from "types";
+import { ElementType, HeaderIcon, NumberFieldTemplate } from "types";
 import { ReactNode } from "react";
 
 export const elementObject: {
@@ -92,21 +101,30 @@ export const elementObject: {
     ],
   },
   [ElementType.Paragraph]: {
-    description: "A paragraph of text",
+    description: "A paragraph of text for content.",
     variants: [
       <ParagraphElement
         formkey="5418"
         element={{
           type: ElementType.Paragraph,
           id: "id-paragraph",
-          text: "ParagraphElement",
+          text: "Useful for explanations or instructions.. lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         }}
       />,
     ],
   },
   [ElementType.Divider]: {
     description: "A horizontal line to separate content",
-    variants: [<Divider />],
+    variants: [
+      <DividerElement
+        element={{
+          type: ElementType.NestedHeading,
+          id: "",
+          text: "",
+        }}
+        formkey={""}
+      />,
+    ],
   },
   [ElementType.Accordion]: {
     description: "A collapsible section for content",
@@ -122,6 +140,98 @@ export const elementObject: {
           I am the content of the third accordion item.
         </AccordionItem>
       </Accordion>,
+    ],
+  },
+  [ElementType.Dropdown]: {
+    description: "A dropdown field for selecting options",
+    variants: [
+      <DropdownField
+        formkey="5419"
+        element={{
+          type: ElementType.Dropdown,
+          id: "id-dropdown",
+          label: "DropdownField",
+          options: [
+            { value: "Option 1", label: "Option 1" },
+            { value: "Option 2", label: "Option 2" },
+            { value: "Option 3", label: "Option 3" },
+          ],
+        }}
+      />,
+    ],
+  },
+  [ElementType.Radio]: {
+    description: "A radio button field for selecting one option",
+    variants: [
+      <RadioField
+        formkey="5420"
+        element={{
+          type: ElementType.Radio,
+          id: "id-radio",
+          label: "RadioField",
+          choices: [
+            { value: "Option 1", label: "Option 1" },
+            { value: "Option 2", label: "Option 2" },
+            { value: "Option 3", label: "Option 3" },
+          ],
+        }}
+      />,
+    ],
+  },
+  [ElementType.ButtonLink]: {
+    description: "A button that acts as a link",
+    variants: [
+      <ButtonLinkElement
+        formkey="5421"
+        element={{
+          type: ElementType.ButtonLink,
+          id: "id-buttonlink",
+          label: "ButtonLinkElement",
+          to: "why-is-this-undefined-three-times",
+        }}
+      />,
+    ],
+  },
+  [ElementType.Date]: {
+    description: "A field for selecting a date",
+    variants: [
+      <DateField
+        formkey="5422"
+        element={{
+          type: ElementType.Date,
+          id: "id-date-field",
+          label: "DateField",
+          helperText: "DateFieldElement is used to select a date.",
+        }}
+      />,
+    ],
+  },
+  ["SubHeaderMeasure"]: {
+    description: "A subheader for measures",
+    variants: [
+      <SubHeaderMeasureElement
+        formkey="5423"
+        element={{
+          type: ElementType.SubHeader,
+          id: "id-subheader",
+          text: "SubHeaderElement",
+        }}
+      />,
+    ],
+  },
+  [ElementType.NumberField]: {
+    description: "A field for entering numbers",
+    variants: [
+      <TextField
+        formkey="5424"
+        element={
+          {
+            type: ElementType.NumberField,
+            id: "id-number-field",
+            label: "Enter a number",
+          } as NumberFieldTemplate
+        }
+      />,
     ],
   },
 };
