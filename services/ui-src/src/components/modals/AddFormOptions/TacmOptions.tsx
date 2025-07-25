27fbcad2140b26512d92261b3ebe27e89a0ae1e6
benmartin-coforma <Ben.Martin@coforma.io>
2025-07-25 11:16:33 -0600

    Remove RHF from AddEditReportModal, AdminBannerForm

--- services/ui-src/src/components/modals/AddFormOptions/TacmOptions.tsx
+++ services/ui-src/src/components/modals/AddFormOptions/TacmOptions.tsx
@@ -1,15 +1,8 @@
-import { AddEditReportModalOptions } from "../AddEditReportModal";
-
-const verbiage = {
-  reportName: "TACM Report",
-  yearSelect: "Select the TACM measurement year.",
-  shortName: "TACM",
-  sampleName: "HCBS TACM Report for 2026",
+export default {
+  verbiage: {
+    reportName: "TACM Report",
+    yearSelect: "Select the TACM measurement year.",
+    shortName: "TACM",
+    sampleName: "HCBS TACM Report for 2026",
+  },
 };
-
-export const TacmOptions = (): AddEditReportModalOptions => ({
-  verbiage: verbiage,
-  reportOptions: {},
-  optionsElements: null,
-  parseFormDataOptions: () => ({}),
-});
