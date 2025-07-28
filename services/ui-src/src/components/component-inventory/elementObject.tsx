d72386f7745c1a01f0224fb713da7203cc327722
Rocio De Santiago <rociodesantiago@CoformaSantiago.lan>
2025-07-28 17:05:01 -0500

    clean up

--- services/ui-src/src/components/component-inventory/elementObject.tsx
+++ services/ui-src/src/components/component-inventory/elementObject.tsx
@@ -9,7 +9,6 @@ import {
 import { AccordionItem } from "components";
 
 import {
-  ButtonLinkElement,
   DividerElement,
   HeaderElement,
   NestedHeadingElement,
@@ -117,12 +116,11 @@ export const elementObject: {
     description: "A horizontal line to separate content",
     variants: [
       <DividerElement
+        formkey="5419"
         element={{
-          type: ElementType.NestedHeading,
-          id: "",
-          text: "",
+          type: ElementType.Divider,
+          id: "id-divider",
         }}
-        formkey={""}
       />,
     ],
   },
@@ -146,15 +144,15 @@ export const elementObject: {
     description: "A dropdown field for selecting options",
     variants: [
       <DropdownField
-        formkey="5419"
+        formkey="5420"
         element={{
           type: ElementType.Dropdown,
           id: "id-dropdown",
           label: "DropdownField",
           options: [
-            { value: "Option 1", label: "Option 1" },
-            { value: "Option 2", label: "Option 2" },
-            { value: "Option 3", label: "Option 3" },
+            { value: "dropdown option 1", label: "dropdown option 1" },
+            { value: "dropdown option 2", label: "dropdown option 2" },
+            { value: "dropdown option 3", label: "dropdown option 3" },
           ],
         }}
       />,
@@ -164,30 +162,16 @@ export const elementObject: {
     description: "A radio button field for selecting one option",
     variants: [
       <RadioField
-        formkey="5420"
+        formkey="5421"
         element={{
           type: ElementType.Radio,
           id: "id-radio",
           label: "RadioField",
           choices: [
-            { value: "Option 1", label: "Option 1" },
-            { value: "Option 2", label: "Option 2" },
-            { value: "Option 3", label: "Option 3" },
+            { value: "radio option 1", label: "radio option 1" },
+            { value: "radio option 2", label: "radio option 2" },
+            { value: "radio option 3", label: "radio option 3" },
           ],
-        }}
-      />,
-    ],
-  },
-  [ElementType.ButtonLink]: {
-    description: "A button that acts as a link",
-    variants: [
-      <ButtonLinkElement
-        formkey="5421"
-        element={{
-          type: ElementType.ButtonLink,
-          id: "id-buttonlink",
-          label: "ButtonLinkElement",
-          to: "why-is-this-undefined-three-times",
         }}
       />,
     ],
@@ -196,7 +180,7 @@ export const elementObject: {
     description: "A field for selecting a date",
     variants: [
       <DateField
-        formkey="5422"
+        formkey="5423"
         element={{
           type: ElementType.Date,
           id: "id-date-field",
@@ -210,7 +194,7 @@ export const elementObject: {
     description: "A subheader for measures",
     variants: [
       <SubHeaderMeasureElement
-        formkey="5423"
+        formkey="5424"
         element={{
           type: ElementType.SubHeader,
           id: "id-subheader",
@@ -223,15 +207,29 @@ export const elementObject: {
     description: "A field for entering numbers",
     variants: [
       <TextField
-        formkey="5424"
+        formkey="5425"
         element={
           {
             type: ElementType.NumberField,
             id: "id-number-field",
             label: "Enter a number",
+            helperText: "Helper text is optional",
+          } as NumberFieldTemplate
+        }
+      />,
+      <TextField
+        formkey="5426"
+        element={
+          {
+            type: ElementType.NumberField,
+            id: "id-number-field-required",
+            label: "NumberField with required validation",
+            helperText: "This field is required.",
+            required: true,
           } as NumberFieldTemplate
         }
       />,
     ],
   },
+  // ButtonLinkElement needs ReportType, state, and reportId
 };
