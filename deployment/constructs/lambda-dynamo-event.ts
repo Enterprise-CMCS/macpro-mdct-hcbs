753d3a406aa5c94ed636eab932c097f41fb0bf4d
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2025-08-21 07:19:16 -0400

    Update lambda-dynamo-event.ts

--- deployment/constructs/lambda-dynamo-event.ts
+++ deployment/constructs/lambda-dynamo-event.ts
@@ -28,7 +28,6 @@ export class LambdaDynamoEventSource extends Construct {
     const {
       additionalPolicies = [],
       environment = {},
-      handler,
       memorySize = 1024,
       tables,
       stackName,
