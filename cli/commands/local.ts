5688a3098e183dbba06e63599d4cbae08ea97ce6
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2025-09-23 14:49:49 -0400

    cleanup

--- cli/commands/local.ts
+++ cli/commands/local.ts
@@ -39,7 +39,7 @@ export const local = {
       throw "LocalStack needs to be running.";
     }
 
-    process.env.AWS_DEFAULT_REGION = "us-east-1";
+    process.env.AWS_DEFAULT_REGION = region;
     process.env.AWS_ACCESS_KEY_ID = "localstack";
     process.env.AWS_SECRET_ACCESS_KEY = "localstack"; // pragma: allowlist secret
     process.env.AWS_ENDPOINT_URL = "http://localhost.localstack.cloud:4566";
@@ -63,8 +63,6 @@ export const local = {
         "yarn",
         "cdklocal",
         "deploy",
-        "--context",
-        "stage=prerequisites",
         "--app",
         '"npx --prefix deployment tsx deployment/local/prerequisites.ts"',
       ],
@@ -77,8 +75,6 @@ export const local = {
         "yarn",
         "cdklocal",
         "deploy",
-        "--context",
-        "stage=prerequisites",
         "--app",
         '"npx --prefix deployment tsx deployment/prerequisites.ts"',
       ],
