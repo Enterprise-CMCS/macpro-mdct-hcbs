edcc4b518fbe80a8547f921ae2ccde25d4c81932
Filipp Gorbunov <filippgorb@gmail.com>
2025-10-01 16:24:43 -0700

    pcp report modal wording

--- services/ui-src/src/components/modals/AddEditReportModal.test.tsx
+++ services/ui-src/src/components/modals/AddEditReportModal.test.tsx
@@ -185,7 +185,7 @@ describe("Test AddEditReportModal types", () => {
     { type: ReportType.QMS, text: "Quality Measure Set Report" },
     { type: ReportType.TACM, text: "TACM Report" },
     { type: ReportType.CI, text: "Critical Incident Report" },
-    { type: ReportType.PCP, text: "Person-Centered Planning" },
+    { type: ReportType.PCP, text: "Person-Centered Planning Report" },
   ])("$type report type renders a title", ({ type, text }) => {
     render(
       <RouterWrappedComponent>
