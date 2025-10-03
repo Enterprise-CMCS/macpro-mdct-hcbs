491f3a8698123e293b7ea20374e3dad2263d2734
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2025-10-03 15:05:21 -0400

    Update to ESM

--- services/topics/handlers/createTopics.js
+++ services/topics/handlers/createTopics.js
@@ -1,5 +1,5 @@
-const topics = require("../libs/topics-lib.js");
-const condensedTopicList = require("../config.js");
+import { createTopics } from "../libs/topics-lib.js";
+import condensedTopicList from "../config.js";
 
 /**
  * String in the format of `--${event.project}--${event.stage}--`
@@ -7,7 +7,6 @@ const condensedTopicList = require("../config.js");
  * Only used for temp branches for easy identification and cleanup.
  */
 const namespace = process.env.topicNamespace;
-const brokers = process.env.brokerString?.split(",") ?? [];
 
 /**
  * Handler triggered on deploy to create known topics in bigmac
@@ -15,7 +14,7 @@ const brokers = process.env.brokerString?.split(",") ?? [];
  * @param {*} _context
  * @param {*} _callback
  */
-exports.handler = async function (event, _context, _callback) {
+export const handler = async (event, _context, _callback) => {
   console.log("Received event:", JSON.stringify(event, null, 2)); // eslint-disable-line no-console
 
   const desiredTopicConfigs = condensedTopicList.flatMap((element) =>
@@ -26,5 +25,5 @@ exports.handler = async function (event, _context, _callback) {
     }))
   );
 
-  await topics.createTopics(brokers, desiredTopicConfigs);
+  await createTopics(process.env.brokerString.split(","), desiredTopicConfigs);
 };
