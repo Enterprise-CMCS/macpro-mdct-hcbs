8541e28105fc09967b94c9d0a77979f9940c8498
ailZhou <aileen.zhou@coforma.io>
2025-10-03 12:57:51 -0400

    added children elements to pdf

--- services/ui-src/src/components/export/ExportedReportWrapper.tsx
+++ services/ui-src/src/components/export/ExportedReportWrapper.tsx
@@ -2,6 +2,7 @@ import { Flex } from "@chakra-ui/react";
 import {
   FormPageTemplate,
   MeasurePageTemplate,
+  PageElement,
   ParentPageTemplate,
   ReviewSubmitTemplate,
 } from "types";
@@ -38,26 +39,33 @@ export const ExportedReportWrapper = ({ section }: Props) => {
 
   if (filteredElements == undefined) return null;
 
-  const elements =
-    filteredElements?.map((element) => {
-      //if the element is a radio, replace the answer with a the label text
-      const modifiedElement = { ...element };
-      if (modifiedElement.type === "radio") {
-        modifiedElement.answer = modifiedElement.choices.find(
-          (choice) => choice.value === modifiedElement.answer
-        )?.label;
-      }
+  //if the element is a radio, replace the answer with a the label text and get the children elements
+  const expandElements: PageElement[] = [];
+  filteredElements.forEach((element) => {
+    const child = [element];
+    if (element.type === "radio") {
+      //Note: answer is be modified from key value to label value from this point onward, if that becomes a problem in the future expand it
+      element.answer = element.choices.find(
+        (choice) => choice.value === element.answer
+      )?.label;
+      child.push(
+        ...element.choices
+          .flatMap((choice) => choice.checkedChildren)
+          .filter((choice) => choice != undefined)
+      );
+    }
+    expandElements.push(...child);
+  });
 
+  const elements =
+    expandElements?.map((element) => {
       return {
         indicator: "label" in element ? element.label ?? "" : "",
         helperText:
           "helperText" in element && element.helperText
             ? isValidHelperText(element.helperText)
             : "",
-        response: renderElements(
-          section as MeasurePageTemplate,
-          modifiedElement
-        ),
+        response: renderElements(section as MeasurePageTemplate, element),
         type: element.type ?? "",
         required: "required" in element ? element.required : false,
       };
