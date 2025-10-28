642f2c8954f6392b2c8263a3df3289cffe3bf3cb
ailZhou <127151429+ailZhou@users.noreply.github.com>
2025-10-28 09:16:34 -0400

    [CMDCT-4997] PCP - PCP-2 (#431)

--- services/app-api/forms/2026/elements.ts
+++ services/app-api/forms/2026/elements.ts
@@ -548,21 +548,49 @@ export const habilitationRate: NdrBasicTemplate = {
   displayRateAsPercent: true,
 };
 
+export const minPerformanceExplanationField: TextAreaBoxTemplate = {
+  type: ElementType.TextAreaField,
+  id: "why-not-minimum",
+  helperText:
+    "The data entered indicates this measure does not meet the 90% Minimum Performance Level.",
+  label: "Explain reason for not meeting Minimum Performance Level.",
+  required: true,
+};
+
 export const beneficiariesRate: NdrBasicTemplate = {
   type: ElementType.NdrBasic,
   id: "beneficiaries-rate",
   required: true,
   hintText: {
     numHint:
-      "Number of beneficiaries who had their person-centered service plan reviewed, and updated as appropriate, based upon a reassessment of functional need, within the measurement period, across all applicable HCBS programs.",
+      "Number of beneficiaries who had a reassessment of functional need completed at least once within the measurement period across all applicable HCBS programs.",
     denomHint:
-      "Number of beneficiaries who were due for an annual reassessment within the measurement period, across all applicable HCBS programs. ",
+      "Number of beneficiaries who were due for an annual reassessment within the measurement period across all applicable HCBS programs.",
     rateHint:
-      "Auto-calculates. Percentage of beneficiaries continuously enrolled for at least 365 days in an applicable HCBS program who had their person-centered service plan reviewed, and updated as appropriate, based on a reassessment of functional need at least once within the measurement period, across all applicable HCBS programs. ",
+      "Auto-calculates. Percentage of beneficiaries continuously enrolled in an applicable HCBS program for at least 365 days for whom a reassessment of functional need was completed at least once within the measurement period, across all applicable HCBS programs.",
   },
   multiplier: 100,
   displayRateAsPercent: true,
   minPerformanceLevel: 90,
+  conditionalChildren: [minPerformanceExplanationField],
+};
+
+export const beneficiariesReviewedRate: NdrBasicTemplate = {
+  type: ElementType.NdrBasic,
+  id: "pcp-2-rate",
+  required: true,
+  hintText: {
+    numHint:
+      "Number of beneficiaries who had their person-centered service plan reviewed, and updated as appropriate, based upon a reassessment of functional need, within the measurement period, across all applicable HCBS programs.",
+    denomHint:
+      "Number of beneficiaries who were due for an annual reassessment within the measurement period, across all applicable HCBS programs.",
+    rateHint:
+      "Auto-calculates. Percentage of beneficiaries continuously enrolled for at least 365 days in an applicable HCBS program who had their person-centered service plan reviewed, and updated as appropriate, based on a reassessment of functional need at least once within the measurement period, across all applicable HCBS programs.",
+  },
+  multiplier: 100,
+  displayRateAsPercent: true,
+  minPerformanceLevel: 90,
+  conditionalChildren: [minPerformanceExplanationField],
 };
 
 // State Sampling Methodology Radio Question
