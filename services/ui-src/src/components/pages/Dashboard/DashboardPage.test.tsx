b280ca625c7c351566571ef7643f8de229549c99
Filipp Gorbunov <filippgorb@gmail.com>
2025-11-19 15:51:29 -0800

    increased DashboardPage test coverage to 100%

--- services/ui-src/src/components/pages/Dashboard/DashboardPage.test.tsx
+++ services/ui-src/src/components/pages/Dashboard/DashboardPage.test.tsx
@@ -9,7 +9,9 @@ import {
 import { useStore } from "utils";
 import { getReportsForState } from "utils/api/requestMethods/report";
 import { Report } from "types";
-// TODO: Add tests for report filtering when we get more than one year as an option
+import { useParams } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
+
 window.HTMLElement.prototype.scrollIntoView = jest.fn();
 
 jest.mock("utils/other/useBreakpoint", () => ({
@@ -42,6 +44,17 @@ jest.mock("utils/api/requestMethods/report", () => ({
       lastEditedBy: "Mock User",
       status: "Not started",
       name: "Mock Report Name",
+      year: 2026,
+    } as Report,
+    {
+      id: "QMSCO123",
+      type: "QMS",
+      state: "CO",
+      lastEdited: new Date("2024-10-24T08:31:54").valueOf(),
+      lastEditedBy: "Mock User",
+      status: "Not started",
+      name: "Mock Report 2027",
+      year: 2027,
     } as Report,
   ]),
 }));
@@ -81,12 +94,56 @@ describe("DashboardPage with state user", () => {
     ).toBeVisible();
   });
 
+  it("should not call reloadReports if no reportType passed in", async () => {
+    (useParams as jest.Mock)
+      .mockResolvedValueOnce(null)
+      .mockResolvedValueOnce(null);
+
+    render(dashboardComponent);
+    expect(getReportsForState).toHaveBeenCalledTimes(0);
+  });
+
   it("should render report data", async () => {
     const { container } = render(dashboardComponent);
     await waitFor(() => {
       expect(getReportsForState).toHaveBeenCalled();
       expect(screen.getByText("Mock Report Name")).toBeInTheDocument();
     });
+
+    const table = container.querySelector("table")!;
+    const columns = [...table.querySelectorAll("tr th")].map(
+      (th) => th.textContent!
+    );
+    const rows = [...table.querySelectorAll("tbody tr")].map((tr) => [
+      ...tr.querySelectorAll("td"),
+    ]);
+
+    expect(columns.length).toBeGreaterThanOrEqual(4);
+    expect(rows.length).toBe(2);
+
+    const cellContent = (columnName: string) => {
+      const columnIndex = columns.indexOf(columnName);
+      if (columnIndex < 0) throw new Error(`Could not find '${columnName}'`);
+      const cell = rows[0][columnIndex];
+      return cell.textContent;
+    };
+    expect(cellContent("Submission name")).toBe("Mock Report Name");
+    expect(cellContent("Last edited")).toBe("10/24/2024");
+    expect(cellContent("Edited by")).toBe("Mock User");
+  });
+
+  it("should be able to filter reports", async () => {
+    const { container } = render(dashboardComponent);
+    await waitFor(() => {
+      expect(getReportsForState).toHaveBeenCalled();
+      expect(screen.getByText("Mock Report Name")).toBeInTheDocument();
+    });
+
+    await userEvent.selectOptions(
+      screen.queryAllByLabelText("Filter by Year")[0],
+      "2026"
+    );
+    await userEvent.click(screen.getByText("Filter"));
 
     const table = container.querySelector("table")!;
     const columns = [...table.querySelectorAll("tr th")].map(
@@ -108,6 +165,21 @@ describe("DashboardPage with state user", () => {
     expect(cellContent("Submission name")).toBe("Mock Report Name");
     expect(cellContent("Last edited")).toBe("10/24/2024");
     expect(cellContent("Edited by")).toBe("Mock User");
+    expect(screen.queryByText("Mock Report 2027")).not.toBeInTheDocument();
+  });
+
+  it("should be able to open the modal to start new report", async () => {
+    render(dashboardComponent);
+    await waitFor(() => {
+      expect(getReportsForState).toHaveBeenCalled();
+      expect(screen.getByText("Mock Report Name")).toBeInTheDocument();
+    });
+
+    await userEvent.click(screen.getByText("Start Quality Measure Set Report"));
+
+    expect(
+      screen.getByText("Add new Quality Measure Set Report")
+    ).toBeInTheDocument();
   });
 });
 
