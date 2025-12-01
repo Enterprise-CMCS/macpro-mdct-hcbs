c71a4d9e344c59bc80e0d2ca1a53fda15687e717
rocio-desantiago <rocio.de-santiago@coforma.io>
2025-12-01 15:07:54 -0600

    [HCBS] QMS - Add "User Guide and Help File" to Dashboard (#482)
    Co-authored-by: Rocio De Santiago <rociodesantiago@CoformaSantiago.lan>

--- services/ui-src/src/components/cards/ReportIntroCardActions.tsx
+++ services/ui-src/src/components/cards/ReportIntroCardActions.tsx
@@ -1,5 +1,6 @@
 import { Button, Flex, Image, Link } from "@chakra-ui/react";
 import nextIcon from "assets/icons/arrows/icon_arrow_next_white.svg";
+import externalLinkIcon from "assets/icons/externalLink/icon_external_link_main.svg";
 import { useNavigate } from "react-router-dom";
 import { ReportType, isReportType } from "types";
 import { useStore } from "utils";
@@ -29,8 +30,29 @@ export const ReportIntroCardActions = ({ reportType }: Props) => {
     }
   };
 
+  const helpFiles: { [key in ReportType]?: string } = {
+    QMS: "CMS_QMS_User_Guide_and_Help_File.pdf",
+  };
+
   return (
-    <Flex sx={sx.actionsFlex}>
+    <Flex sx={reportType in helpFiles ? sx.actionsFlex : sx.actionsFlexEnd}>
+      {reportType in helpFiles && (
+        <Button
+          as={Link}
+          variant="link"
+          sx={sx.userGuideDownloadButton}
+          href={`${window.location.origin}/${helpFiles[reportType]}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          User Guide and Help File
+          <Image
+            src={externalLinkIcon}
+            sx={sx.externalLinkIcon}
+            alt="External Link Opens in New Tab"
+          />
+        </Button>
+      )}
       <Button
         as={Link}
         variant={"primary"}
@@ -55,6 +77,14 @@ interface Props {
 const sx = {
   actionsFlex: {
     flexFlow: "no-wrap",
+    justifyContent: "space-between",
+    marginY: "spacer3",
+    ".mobile &": {
+      flexDirection: "column",
+    },
+  },
+  actionsFlexEnd: {
+    flexFlow: "no-wrap",
     justifyContent: "end",
     marginY: "spacer3",
     ".mobile &": {
@@ -72,5 +102,21 @@ const sx = {
       textDecoration: "none",
       backgroundColor: "palette.primary_darker",
     },
+  },
+  userGuideDownloadButton: {
+    justifyContent: "start",
+    marginRight: "1rem",
+    padding: "0",
+    span: {
+      marginLeft: "0rem",
+      marginRight: "0.5rem",
+    },
+    ".mobile &": {
+      marginRight: "0",
+    },
+  },
+  externalLinkIcon: {
+    marginLeft: "0.5rem",
+    height: "1rem",
   },
 };
