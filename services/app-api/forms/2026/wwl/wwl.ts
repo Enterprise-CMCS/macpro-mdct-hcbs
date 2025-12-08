c92c51bfb98e07a1adcde557a870649508a77c8c
Ajaita Saini <ajaita.saini@coforma.io>
2025-12-08 11:05:58 -0500

    wip

--- services/app-api/forms/2026/wwl/wwl.ts
+++ services/app-api/forms/2026/wwl/wwl.ts
@@ -58,28 +58,12 @@ export const wwlReportTemplate: ReportBase = {
             "Enter an email address for the person or position above.  Department or program-wide email addresses are allowed.",
         },
         {
-          type: ElementType.Radio,
-          id: "report-coverage-waivers-programs",
-          label:
-            "Does this report cover all the programs that are required under the relevant authorities?",
-          choices: [
-            { label: "Yes", value: "yes" },
-            {
-              label: "No",
-              value: "no",
-              checkedChildren: [
-                {
-                  type: ElementType.TextAreaField,
-                  id: "included-waivers-programs",
-                  label: "Which programs and waivers are included?",
-                  required: true,
-                  helperText:
-                    "Please specify all the 1915(c) waivers, 1915(i), 1915(j), and 1915(k) State plan benefits, as well as any 1115 demonstrations that include HCBS, that you are including in this report. Include the program name and control numbers in your response.",
-                },
-              ],
-            },
-          ],
+          type: ElementType.NumberField,
+          id: "number-of-individuals",
+          label: "Number of individuals included on the waiting list",
           required: true,
+          helperText:
+            "Enter the total number of individuals on the waiting list.  Use whole numbers only.  Include all individuals who were enrolled at any point during the measurement period.",
         },
       ],
     },
