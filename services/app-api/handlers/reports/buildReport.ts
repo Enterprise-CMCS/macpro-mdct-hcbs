bf6aa1ce6a0f7e9f395cf908ea722f7b7c515ead
ailZhou <aileen.zhou@coforma.io>
2025-12-23 10:49:51 -0500

    added waiver to all reports

--- services/app-api/handlers/reports/buildReport.ts
+++ services/app-api/handlers/reports/buildReport.ts
@@ -16,11 +16,33 @@ import {
   PageStatus,
   isReportWithMeasuresTemplate,
   ElementType,
+  WAIVER,
 } from "../../types/reports";
 import { User } from "../../types/types";
 import { validateReportPayload } from "../../utils/reportValidation";
 import { logger } from "../../libs/debug-lib";
 import { StateAbbr } from "../../utils/constants";
+
+const generateWaiverCheckboxes = (report: Report, waiverList: WAIVER[]) => {
+  return [...report.pages].map((page) => {
+    if (page.elements) {
+      const waiver = page.elements.find(
+        (element) => element.id == "waivers-list-checkboxes"
+      );
+      if (waiver && waiver.type === ElementType.Checkbox) {
+        waiver.choices = waiverList.map((waiver) => {
+          return {
+            label: `${waiver.waiverType}: ${waiver.controlNumber} ${waiver.programTitle}`,
+            value: waiver.id,
+            checked: true,
+          };
+        });
+        waiver.answer = waiverList.map((waiver) => waiver.id);
+      }
+    }
+    return page;
+  });
+};
 
 export const buildReport = async (
   reportType: ReportType,
@@ -83,25 +105,12 @@ export const buildReport = async (
   }
 
   //certain checkbox forms that utilize waivers need to have their checkboxes generate during form generation
-  if (report.type === ReportType.CI) {
-    report.pages = [...report.pages].map((page) => {
-      if (page.elements) {
-        const waiver = page.elements.find(
-          (element) => element.id == "waivers-not-included"
-        );
-        if (waiver && waiver.type === ElementType.Checkbox) {
-          waiver.choices = waiverList.map((waiver) => {
-            return {
-              label: `${waiver.waiverType}: ${waiver.controlNumber} ${waiver.programTitle}`,
-              value: waiver.id,
-              checked: true,
-            };
-          });
-          waiver.answer = waiverList.map((waiver) => waiver.id);
-        }
-      }
-      return page;
-    });
+  if (
+    [ReportType.CI, ReportType.PCP, ReportType.TACM, ReportType.QMS].includes(
+      report.type
+    )
+  ) {
+    report.pages = generateWaiverCheckboxes(report, waiverList);
   }
 
   /**
