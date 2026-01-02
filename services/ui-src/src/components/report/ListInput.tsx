1e0b265c1e525bbfee7630421f4f4cfbd3284220
ailZhou <aileen.zhou@coforma.io>
2026-01-02 14:36:35 -0500

    add functionality to list input

--- services/ui-src/src/components/report/ListInput.tsx
+++ services/ui-src/src/components/report/ListInput.tsx
@@ -1,7 +1,67 @@
 import { ListInputTemplate } from "types";
 import { PageElementProps } from "../report/Elements";
-import { Box } from "@chakra-ui/react";
+import { Button, HStack, Image } from "@chakra-ui/react";
+import { TextField } from "@cmsgov/design-system";
+import { useState } from "react";
+import cancelPrimary from "assets/icons/cancel/icon_cancel_primary.svg";
+import addPrimary from "assets/icons/add/icon_add_blue.svg";
 
 export const ListInput = (props: PageElementProps<ListInputTemplate>) => {
-  return <Box>list input display</Box>;
+  const { label, fieldLabel, helperText, buttonText, answers } = props.element;
+  const [displayValue, setDisplayValue] = useState(answers ?? []);
+
+  const onChangeHandler = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    index: number
+  ) => {
+    const rawValue = event.target.value;
+    const newDisplay = [...displayValue];
+    newDisplay[index] = rawValue;
+    setDisplayValue(newDisplay);
+  };
+
+  const onAddHandler = () => {
+    const newDisplay = [...displayValue];
+    newDisplay.push("");
+    setDisplayValue(newDisplay);
+  };
+
+  const onRemoveHandler = (index: number) => {
+    const newDisplay = [...displayValue];
+    newDisplay.splice(index, 1);
+    setDisplayValue(newDisplay);
+  };
+
+  return (
+    <fieldset className="ds-c-fieldset" key="list-input-field">
+      <legend className="ds-c-label">{label}</legend>
+      <p className="ds-c-hint">{helperText} </p>
+      {displayValue?.map((field, index) => (
+        <HStack
+          alignItems="flex-end"
+          mt="1rem"
+          key={`list-item-stack-${index}`}
+        >
+          <TextField
+            key={`list-item-${index}`}
+            name={`list-item-${index}`}
+            label={fieldLabel}
+            value={field}
+            onChange={(evt) => onChangeHandler(evt, index)}
+          ></TextField>
+          <Button variant="unstyled" onClick={() => onRemoveHandler(index)}>
+            <Image src={cancelPrimary} alt="Cancel" />
+          </Button>
+        </HStack>
+      ))}
+      <Button
+        mt="1rem"
+        variant="outline"
+        leftIcon={<Image src={addPrimary} alt="Add" />}
+        onClick={onAddHandler}
+      >
+        {buttonText}
+      </Button>
+    </fieldset>
+  );
 };
