544b172f6995ce8541f67149837f82c872b2717a
benmartin-coforma <Ben.Martin@coforma.io>
2026-02-25 10:10:34 -0700

    Remove update support from createBanner endpoint

--- services/app-api/handlers/banners/create.ts
+++ services/app-api/handlers/banners/create.ts
@@ -1,12 +1,11 @@
 import { handler } from "../../libs/handler-lib";
-import { getBanner, putBanner } from "../../storage/banners";
+import { putBanner } from "../../storage/banners";
 import { randomUUID } from "node:crypto";
 import { error } from "../../utils/constants";
-import { badRequest, ok, forbidden } from "../../libs/response-lib";
+import { badRequest, created, forbidden } from "../../libs/response-lib";
 import { canWriteBanner } from "../../utils/authorization";
 import { emptyParser } from "../../libs/param-lib";
 import { isValidBanner } from "../../utils/bannerValidation";
-import { BannerShape } from "../../types/banner";
 
 export const createBanner = handler(emptyParser, async (request) => {
   const user = request.user;
@@ -21,20 +20,15 @@ export const createBanner = handler(emptyParser, async (request) => {
 
   const currentTime = new Date().toISOString();
 
-  let existingBanner: BannerShape | undefined;
-  if ("key" in request.body && "string" === typeof request.body.key) {
-    existingBanner = await getBanner(request.body.key);
-  }
-
   const newBanner = {
     ...request.body,
-    key: existingBanner?.key ?? randomUUID(),
-    createdAt: existingBanner?.createdAt ?? currentTime,
+    key: randomUUID(),
+    createdAt: currentTime,
     lastAltered: currentTime,
     lastAlteredBy: user.fullName,
   };
 
   await putBanner(newBanner);
 
-  return ok(newBanner);
+  return created(newBanner);
 });
