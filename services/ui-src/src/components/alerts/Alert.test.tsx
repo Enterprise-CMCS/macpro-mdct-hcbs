dbd7b530d40577f42144400647c04c6a2ea9c1b8
benmartin-coforma <Ben.Martin@coforma.io>
2026-03-03 14:20:45 -0700

    Extra test coverage for <Alert>

--- services/ui-src/src/components/alerts/Alert.test.tsx
+++ services/ui-src/src/components/alerts/Alert.test.tsx
@@ -1,6 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import { Alert } from "components";
+import { AlertTypes } from "types";
 import { testA11y } from "utils/testing/commonTests";
+
+// jest.mock("react", () => ({
+//   ...jest.requireActual("react"),
+//   useRef: jest.fn().mockReturnValue({
+//     current: {
+//       scrollIntoView: jest.fn(),
+//       focus: jest.fn(),
+//     },
+//   }),
+// }));
+// const mockRef = useRef(null).current as any;
 
 /** The path to our alert SVG, as injected by jest */
 const alertIcon = "test-file-stub";
@@ -12,6 +24,10 @@ const alertComponent = (
 );
 
 describe("<Alert />", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   it("should render correctly", () => {
     render(alertComponent);
     expect(screen.getByRole("alert")).toBeVisible();
@@ -26,6 +42,19 @@ describe("<Alert />", () => {
   it("should hide the icon when appropriate", () => {
     render(<Alert title="mock title" showIcon={false} />);
     expect(screen.queryByRole("img")).not.toBeInTheDocument();
+  });
+
+  it("should focus error alerts", () => {
+    const scrollSpy = jest.spyOn(Element.prototype, "scrollIntoView");
+    const focusSpy = jest.spyOn(HTMLElement.prototype, "focus");
+
+    const { rerender } = render(<Alert status={AlertTypes.INFO} title="" />);
+    expect(scrollSpy).not.toHaveBeenCalled();
+    expect(focusSpy).not.toHaveBeenCalled();
+
+    rerender(<Alert status={AlertTypes.ERROR} title="" />);
+    expect(scrollSpy).toHaveBeenCalled();
+    expect(focusSpy).toHaveBeenCalled();
   });
 
   testA11y(alertComponent);
