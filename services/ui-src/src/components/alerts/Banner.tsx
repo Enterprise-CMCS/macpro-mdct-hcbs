0200e7a64d0894197b89f550b514bafdfa4aeea6
Sophie Kwak <sophie.kwak@coforma.io>
2026-03-09 16:19:40 -0700

    styling added to banner a

--- services/ui-src/src/components/alerts/Banner.tsx
+++ services/ui-src/src/components/alerts/Banner.tsx
@@ -1,3 +1,4 @@
+import { Box } from "@chakra-ui/react";
 import { Alert } from "components";
 import { BannerFormData } from "types";
 import { parseHtml } from "utils";
@@ -5,9 +6,25 @@ import { parseHtml } from "utils";
 export const Banner = ({ title, description, link }: Props) => {
   return (
     <Alert title={title} link={link}>
-      {parseHtml(description)}
+      <Box sx={sx}>{parseHtml(description)}</Box>
     </Alert>
   );
 };
 
 type Props = Pick<BannerFormData, "title" | "description" | "link">;
+
+const sx = {
+  "& a, & a:visited": {
+    color: "blue.600",
+    textDecoration: "underline",
+    textDecorationColor: "blue.600",
+  },
+  "& a:hover, & a:visited:hover": {
+    color: "blue.700",
+    textDecorationColor: "blue.700",
+  },
+  "& a:focus, & a:visited:focus": {
+    color: "inherit",
+    textDecorationColor: "blue.600",
+  },
+};
