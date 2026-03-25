3a390494a43c665f7ffba59e5aada02646f99bb8
Rocio De Santiago <rociodesantiago@CoformaSantiago.lan>
2026-03-25 19:16:16 -0400

    forgot an s

--- services/ui-src/src/components/report/StatusTable.tsx
+++ services/ui-src/src/components/report/StatusTable.tsx
@@ -95,7 +95,7 @@ export const StatusTableElement = () => {
           <Tr>
             <Th>Section</Th>
             <Th>Status</Th>
-            <Th>Action</Th>
+            <Th>Actions</Th>
           </Tr>
         </Thead>
         <Tbody>{rows}</Tbody>
