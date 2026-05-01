e5a0cc73c663fc2691a8d8c2136f72fec01f5b88
Sophie Kwak <60756243+sopspops@users.noreply.github.com>
2026-05-01 10:34:32 -0700

    Critical Incident Measures - Update Additional Comment Text (#659)

--- services/app-api/forms/2026/ci/ciElements.ts
+++ services/app-api/forms/2026/ci/ciElements.ts
@@ -4,7 +4,7 @@ export const criticalIncidentCommentsField: TextAreaBoxTemplate = {
   type: ElementType.TextAreaField,
   id: "critical-incident-comments-field",
   helperText:
-    "If applicable, add any notes or comments to provide context to the reported results. If this measure did not meet the minimum performance level, explain here.",
+    "If applicable, add any notes or comments to provide context to the reported results.",
   label: "Additional comments",
   required: false,
 };
