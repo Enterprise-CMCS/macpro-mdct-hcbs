5e882a665f94c40b34741fc77654252fe3987fab
benmartin-coforma <126210497+benmartin-coforma@users.noreply.github.com>
2026-07-28 08:58:33 -0600

    Refactor PDF page iteration (#751)

--- services/ui-src/src/components/pages/Export/ExportedReportPage.test.tsx
+++ services/ui-src/src/components/pages/Export/ExportedReportPage.test.tsx
@@ -1,100 +1,270 @@
 import { screen, render } from "@testing-library/react";
 import { useStore } from "utils";
 import { ExportedReportPage } from "./ExportedReportPage";
-import { PageType, PageStatus } from "types";
-
-jest.mock("utils", () => ({
-  ...jest.requireActual("utils"),
-  useStore: jest.fn(),
-}));
+import { Report, PageTemplate, ElementType, PageType, PageStatus } from "types";
 
 const report = {
   type: "QMS",
   id: "mock-report-id",
   state: "CO",
   name: "mock-title",
-  pages: [
-    { childPageIds: ["1", "2"] },
-    { title: "Section 1", id: "id-1" },
-    { title: "Section 2", id: "id-2" },
-    { title: "Section 3", id: "req-measure-result" },
-    { title: "Section 4", id: "review-submit" },
-    {
-      title: "Section 5",
-      id: "id-5",
-      type: PageType.Measure,
-      required: false,
-      status: PageStatus.NOT_STARTED,
-    },
-    {
-      title: "Section 6",
-      id: "id-6",
-      type: PageType.MeasureResults,
-      status: PageStatus.NOT_STARTED,
-    },
-    {
-      title: "Section 7",
-      id: "id-7",
-      type: PageType.Measure,
-      status: PageStatus.NOT_STARTED,
-      required: true,
-      dependentPages: [
-        {
-          template: "id-7-1",
-          key: "7-dep",
-          linkText: "Section 7 Dependent Page",
-        },
-      ],
-    },
-    {
-      title: "Section 7 Dependent Page",
-      id: "id-7-1",
-      type: PageType.MeasureResults,
-      status: PageStatus.NOT_STARTED,
-    },
-  ],
+  pages: [] as PageTemplate[],
   lastEdited: 1751987780396,
-  lastEditedBy: "last edited",
+  lastEditedBy: "Mrs Editor",
   status: "In progress",
-  options: {
-    cahps: true,
-    nciidd: false,
-    nciad: true,
-    pom: false,
-  },
-};
+  options: { cahps: true, nciidd: false, nciad: true, pom: false },
+} as Report;
+useStore.setState({ report });
+
+const subHeader = (text: string) => ({
+  type: ElementType.SubHeader,
+  id: "",
+  text,
+});
 
 describe("ExportedReportPage", () => {
-  beforeEach(() => {
-    jest.clearAllMocks();
-    (useStore as unknown as jest.Mock).mockReturnValue({
-      report: report,
-    });
-  });
   it("ExportReportPage is visible", () => {
-    render(<ExportedReportPage></ExportedReportPage>);
+    useStore.setState({
+      report: {
+        ...report,
+        pages: [{ id: "root", childPageIds: [] }] as PageTemplate[],
+      },
+    });
+
+    render(<ExportedReportPage />);
+
     const elements = screen.getAllByText(
       "Colorado Quality Measure Set Report for: mock-title"
     );
     expect(elements.length).toBe(2);
+
+    expect(
+      screen.getByRole("row", {
+        name: "Reporting year Last edited Edited by Status",
+      })
+    );
+    expect(
+      screen.getByRole("row", { name: "07/08/2025 Mrs Editor In progress" })
+    );
+
+    expect(screen.getByRole("row", { name: /CAHPS Survey\? Yes/ }));
+    expect(screen.getByRole("row", { name: /NCI-IDD Survey\? No/ }));
+    expect(screen.getByRole("row", { name: /NCI-AD Survey\? Yes/ }));
+    expect(screen.getByRole("row", { name: /POM Survey\? No/ }));
   });
 
   it("Should not render filtered sections", () => {
-    render(<ExportedReportPage></ExportedReportPage>);
-    expect(screen.queryByText("Section 4")).not.toBeInTheDocument();
+    useStore.setState({
+      report: {
+        ...report,
+        pages: [
+          {
+            id: "root",
+            childPageIds: ["review-submit"],
+            elements: [subHeader("Root")],
+          },
+          {
+            id: "review-submit",
+            elements: [subHeader("Review")],
+          },
+        ] as PageTemplate[],
+      },
+    });
+
+    render(<ExportedReportPage />);
+
+    // The root page never renders
+    expect(screen.queryByText(/Root/)).not.toBeInTheDocument();
+
+    // The Review & Submit page never renders
+    expect(screen.queryByText(/Review/)).not.toBeInTheDocument();
   });
 
-  it("Should not render optional measures which are not started", () => {
-    render(<ExportedReportPage></ExportedReportPage>);
-    expect(screen.queryByText("Section 5")).not.toBeInTheDocument();
+  it("Should not render unreferenced sections", () => {
+    useStore.setState({
+      report: {
+        ...report,
+        pages: [
+          { id: "root", childPageIds: ["page-a"] },
+          {
+            id: "page-a",
+            elements: [subHeader("Page Alpha")],
+          },
+          {
+            id: "page-b",
+            elements: [subHeader("Page Beta")],
+          },
+        ] as PageTemplate[],
+      },
+    });
+
+    render(<ExportedReportPage />);
+
+    // Referenced from root
+    expect(screen.queryByText(/Alpha/)).toBeInTheDocument();
+
+    // Never referenced
+    expect(screen.queryByText(/Beta/)).not.toBeInTheDocument();
   });
 
-  it("Should not render measure results which are not started", () => {
-    render(<ExportedReportPage></ExportedReportPage>);
-    expect(screen.queryByText("Section 6")).not.toBeInTheDocument();
+  it("Should render required measures and results with section header", () => {
+    useStore.setState({
+      report: {
+        ...report,
+        pages: [
+          { id: "root", childPageIds: ["req-measure-result"] },
+          {
+            id: "req-measure-result",
+            elements: [subHeader("Fluff")],
+          },
+          {
+            id: "measure-a",
+            type: PageType.Measure,
+            required: true,
+            status: PageStatus.IN_PROGRESS,
+            elements: [subHeader("Measure Alpha")],
+            dependentPages: [
+              { template: "measure-a-ffs" },
+              { template: "measure-a-mltss" },
+            ],
+          },
+          {
+            id: "measure-a-ffs",
+            type: PageType.MeasureResults,
+            status: PageStatus.NOT_STARTED,
+            elements: [subHeader("Alpha FFS Results")],
+          },
+          {
+            id: "measure-a-mltss",
+            type: PageType.MeasureResults,
+            status: PageStatus.IN_PROGRESS,
+            elements: [subHeader("Alpha MLTSS Results")],
+          },
+          {
+            id: "measure-b",
+            type: PageType.Measure,
+            required: true,
+            status: PageStatus.NOT_STARTED,
+            elements: [subHeader("Measure Beta")],
+            dependentPages: [],
+          },
+        ] as PageTemplate[],
+      },
+    });
+
+    render(<ExportedReportPage />);
+
+    // The contents of the Required Measures page don't render
+    expect(screen.queryByText(/Fluff/)).not.toBeInTheDocument();
+
+    // The injected header does render
+    expect(screen.getByText(/Required Measures/)).toBeInTheDocument();
+
+    // The measure does render
+    expect(screen.getByText(/Measure Alpha/)).toBeInTheDocument();
+    // This status doesn't appear to be reporting Alpha FFS results
+    expect(screen.queryByText(/Alpha FFS Results/)).not.toBeInTheDocument();
+    // The MLTSS results have not been filled out
+    expect(screen.getByText(/Alpha MLTSS Results/)).toBeInTheDocument();
+
+    // Even though Beta is not started, it is required, so we show it
+    expect(screen.getByText(/Measure Beta/)).toBeInTheDocument();
   });
-  it("Should render Required Measures Heading", () => {
-    render(<ExportedReportPage></ExportedReportPage>);
-    expect(screen.queryByText("Required Measures")).toBeInTheDocument();
+
+  it("Should not render optional measure header when there is no optional data", () => {
+    useStore.setState({
+      report: {
+        ...report,
+        pages: [
+          { id: "root", childPageIds: ["optional-measure-result"] },
+          {
+            id: "optional-measure-result",
+            elements: [subHeader("Fluff")],
+          },
+          {
+            id: "measure-g",
+            type: PageType.Measure,
+            required: false,
+            status: PageStatus.NOT_STARTED,
+            elements: [subHeader("Measure Gamma")],
+            dependentPages: [],
+          },
+        ] as PageTemplate[],
+      },
+    });
+
+    render(<ExportedReportPage />);
+
+    // The contents of the Optional Measures page don't render
+    expect(screen.queryByText(/Fluff/)).not.toBeInTheDocument();
+
+    // The injected header does not render
+    expect(screen.queryByText(/Optional Measures/)).not.toBeInTheDocument();
+
+    // The measure, being optional and empty, does not render
+    expect(screen.queryByText(/Measure Gamma/)).not.toBeInTheDocument();
+  });
+
+  it("Should render optional section when it has data", () => {
+    useStore.setState({
+      report: {
+        ...report,
+        pages: [
+          { id: "root", childPageIds: ["optional-measure-result"] },
+          { id: "optional-measure-result" },
+          {
+            id: "measure-g",
+            type: PageType.Measure,
+            required: false,
+            status: PageStatus.IN_PROGRESS,
+            elements: [subHeader("Measure Gamma")],
+            dependentPages: [],
+          },
+        ] as PageTemplate[],
+      },
+    });
+
+    render(<ExportedReportPage />);
+
+    // The injected header does render
+    expect(screen.getByText(/Optional Measures/)).toBeInTheDocument();
+
+    // The measure, since it is In Progress, also renders
+    expect(screen.getByText(/Measure Gamma/)).toBeInTheDocument();
+  });
+
+  it("Should render QIP Measure Target pages", () => {
+    useStore.setState({
+      report: {
+        ...report,
+        pages: [
+          { id: "root", childPageIds: ["select-measures"] },
+          {
+            id: "select-measures",
+            elements: [
+              subHeader("Fluff"),
+              {
+                id: "select-measures-table",
+                type: ElementType.QipMeasureTable,
+                answer: [{ pageId: "target-d" }],
+              },
+            ],
+          },
+          {
+            id: "target-d",
+            type: PageType.Standard,
+            elements: [subHeader("Measure Delta Targets")],
+          },
+        ] as PageTemplate[],
+      },
+    });
+
+    render(<ExportedReportPage />);
+
+    // The Select Measures page itself does not render
+    expect(screen.queryByText(/Fluff/)).not.toBeInTheDocument();
+
+    // But the measure target pages it references do render
+    expect(screen.getByText(/Measure Delta Targets/)).toBeInTheDocument();
   });
 });
