dfee444c0926e359efd83af55df6c0cf69471647
benmartin-coforma <Ben.Martin@coforma.io>
2026-08-04 14:14:47 -0600

    Convert DateField to use MultiFormatDateField

--- services/ui-src/src/components/fields/DateField.tsx
+++ services/ui-src/src/components/fields/DateField.tsx
@@ -1,14 +1,9 @@
 import { useEffect, useState } from "react";
-import { Box, Text } from "@chakra-ui/react";
+import { MultiFormatDateField } from "components/cms-extensions/MultiFormatDateField";
 import { parseHtml } from "utils";
-import {
-  SingleInputDateField as CmsdsDateField,
-  TextField as CmsdsTextField,
-} from "@cmsgov/design-system";
 import { PageElementProps } from "../report/Elements";
 import { DateTemplate } from "../../types/report";
 import { validateDate } from "utils/validation/inputValidation";
-import { formatMonthYearInput, formatWithPlaceholders } from "./monthYearInput";
 
 export const DateField = (props: PageElementProps<DateTemplate>) => {
   const dateTextbox = props.element;
@@ -21,37 +16,20 @@ export const DateField = (props: PageElementProps<DateTemplate>) => {
     setDisplayValue(dateTextbox.answer ?? "");
   }, [dateTextbox.answer]);
 
-  const onChangeHandler = (rawValue: string, maskedValue: string) => {
+  const onChangeHandler = (
+    rawValue: string,
+    maskedValue: string,
+    _date: Date | undefined
+  ) => {
     setDisplayValue(rawValue);
     const { isValid, errorMessage } = validateDate(
       rawValue,
       maskedValue,
-      dateTextbox.required
-    );
-    props.updateElement({ answer: isValid ? maskedValue : undefined });
-    setErrorMessage(errorMessage);
-  };
-
-  const onMonthYearChangeHandler = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    const maskedValue = formatMonthYearInput(event.target.value);
-    setDisplayValue(maskedValue);
-    const { isValid, errorMessage } = validateDate(
-      maskedValue,
-      maskedValue,
       dateTextbox.required,
       undefined,
-      "MMYYYY"
+      dateFormat
     );
-
-    if (!isValid) {
-      props.updateElement({ answer: undefined });
-      setErrorMessage(errorMessage);
-      return;
-    }
-
-    props.updateElement({ answer: maskedValue });
+    props.updateElement({ answer: isValid ? maskedValue : undefined });
     setErrorMessage(errorMessage);
   };
 
@@ -65,56 +43,17 @@ export const DateField = (props: PageElementProps<DateTemplate>) => {
       )}
     </>
   );
-  const monthYearHint = (
-    <>
-      {parsedHint}
-      <Text as="span" display="block" sx={sx.monthYearHintText}>
-        {formatWithPlaceholders(displayValue)}
-      </Text>
-    </>
-  );
 
   return (
-    <Box>
-      {dateFormat === "MMYYYY" ? (
-        <Box sx={sx.monthYearInput}>
-          <CmsdsTextField
-            name={dateTextbox.id}
-            label={labelText}
-            onChange={onMonthYearChangeHandler}
-            value={displayValue}
-            hint={monthYearHint}
-            errorMessage={errorMessage}
-            disabled={props.disabled}
-            inputMode="numeric"
-          />
-        </Box>
-      ) : (
-        <CmsdsDateField
-          name={dateTextbox.id}
-          label={labelText}
-          onChange={onChangeHandler}
-          value={displayValue}
-          hint={parsedHint}
-          errorMessage={errorMessage}
-          disabled={props.disabled}
-        />
-      )}
-    </Box>
+    <MultiFormatDateField
+      dateFormat={dateFormat}
+      name={dateTextbox.id}
+      label={labelText}
+      onChange={onChangeHandler}
+      value={displayValue}
+      hint={parsedHint}
+      errorMessage={errorMessage}
+      disabled={props.disabled}
+    />
   );
-};
-
-const sx = {
-  monthYearHintText: {
-    mt: "1",
-    fontSize: "16px",
-    fontFamily: "Menlo, Consolas, Monaco",
-    fontWeight: "500",
-    color: "#5a5a5a",
-  },
-  monthYearInput: {
-    "input:not(.ds-c-choice)": {
-      maxWidth: "12ch",
-    },
-  },
 };
