1d28240e2bfcafa07ebc8ec2712339324d040d1d
Rocio De Santiago <rociodesantiago@CoformaSantiago.lan>
2026-09-02 16:10:58 -0500

    add new page to ima report

--- services/app-api/types/reports.ts
+++ services/app-api/types/reports.ts
@@ -422,7 +422,7 @@ export type DateRangeTemplate = {
     start: string;
     end: string;
   };
-  helperText: string;
+  helperText?: string;
   startHelperText?: string;
   endHelperText?: string;
   dateFormat?: "MMDDYYYY" | "MMYYYY";
