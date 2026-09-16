c247c9bdfbdc7a910f3dd44cb1251c0cac1ab020
Rocio De Santiago <rociodesantiago@CoformaSantiago.lan>
2026-09-16 13:18:36 -0500

    enforce rel for parsing on front end

--- services/ui-src/src/utils/other/parsing.ts
+++ services/ui-src/src/utils/other/parsing.ts
@@ -1,6 +1,12 @@
 import DOMPurify from "dompurify";
 import parse from "html-react-parser";
 
+DOMPurify.addHook("afterSanitizeAttributes", (node) => {
+  if (node.tagName === "A" && node.getAttribute("target") === "_blank") {
+    node.setAttribute("rel", "noopener noreferrer");
+  }
+});
+
 /** Parse HTML string, sanitize, and return React elements. */
 export const parseHtml = (html: string) => {
   const sanitizedHtml = DOMPurify.sanitize(html, {
