d80d09cdb4d60f64c5094361e7a8a033b2c25a05
Sophie Kwak <sophie.kwak@coforma.io>
2026-09-17 14:55:35 -0700

    accordion hover state color changed

--- services/ui-src/src/components/accordions/AccordionItem.tsx
+++ services/ui-src/src/components/accordions/AccordionItem.tsx
@@ -44,6 +44,9 @@ const sx = {
     minHeight: "3.5rem",
     bg: "palette.gray_lightest",
     textAlign: "left",
+    _hover: {
+      bg: "palette.gray_light",
+    },
   },
   accordionPanel: {
     padding: "spacer3",
